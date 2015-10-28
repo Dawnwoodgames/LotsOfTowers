94eb016cf1fab1d0573f9e0511229a181d5e84db
Tim Ververs <timververs@msn.com>
2015-10-28 15:15:37 +0100

    Deathtrigger if you fall of the stage, also created prefab

---
+++ Assets/Scripts/Platform/OffScreenCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LotsOfTowers.Platform
+{
+	public class OffScreenCollider : MonoBehaviour
+	{
+		public Transform spawnPoint;
+
+		void OnCollisionEnter(Collision collision)
+		{
+			if(collision.gameObject.tag == "Player")
+			{
+				try
+				{
+					//Player fell off the stage, reset him to the spawn point
+					collision.gameObject.transform.position = spawnPoint.position;
+				}
+				catch (System.Exception)
+				{
+					throw;
+				}
+			}
+		}
+	}
+}
