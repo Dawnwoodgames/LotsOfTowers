7814b88a5a0ec0690a11d9e6c968bb9b46cee0b9
Lars <larsh1991@hotmail.com>
2015-10-28 12:41:25 +0100

    Third person controller added and free camera movement

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LotsOfTowers {
+	public sealed class GameManager : MonoBehaviour {
+		private static GameManager instance;
+		private Transform spawnPoint;
+
+		public static GameManager Instance {
+			get { return instance; }
+		}
+
+		public Transform SpawnPoint {
+			get { return spawnPoint; }
+		}
+
+		public void Awake() {
+			DontDestroyOnLoad(this);
+			GameManager.instance = this;
+			OnLevelWasLoaded(-1);
+		}
+
+		public void OnLevelWasLoaded(int index) {
+			if (GameObject.Find("Level") != null) {
+				spawnPoint = GameObject.Find("Level/Spawn Point").transform;
+			}
+		}
+	}
+}
---
+++ Assets/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraScript : MonoBehaviour {
+
+    GameObject main;
+
+	// Use this for initialization
+	void Start ()
+    {
+        main = GameObject.Find("Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CameraInput();
+	}
+
+    private void CameraInput()
+    {
+        //main.transform.Rotate(0, Input.GetKey("RightJoystickX") * 2, 0);
+        if (Input.GetKey("e"))
+            main.transform.Rotate(Vector3.down * 2);
+        if (Input.GetKey("q"))
+            main.transform.Rotate(Vector3.up * 2);
+    }
+}
