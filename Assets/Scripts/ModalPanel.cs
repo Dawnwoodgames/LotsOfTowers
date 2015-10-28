13cb6111bf153a6ae3c1ad7bfa505dd4098d9934
Sephy <bob.vankamp@gmail.com>
2015-10-28 15:42:38 +0100

    Canvas added for tooltips

---
+++ Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ModalPanel : MonoBehaviour {
+
+	public Text message;
+	//public Image iconImage;
+	public string closeKey;
+
+	void Awake(){
+		closeKey = "x";
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (closeKey))
+			ClosePanel();
+	}
+
+	// Yes/No/Cancel: A string, a Yes event, a No event and Cancel event
+	public void Tooltip (string message, string closeKey) {
+		this.closeKey = closeKey;
+		//this.iconImage.sprite = icon;
+
+		this.message.text = message;
+
+		//this.iconImage.gameObject.SetActive (false);
+	}
+
+	void ClosePanel () {
+		Destroy (gameObject);
+	}
+}
