bc7f587f3d7973b1077a24f48391864793798d07
Sephy <bob.vankamp@gmail.com>
2015-10-29 13:22:39 +0100

    Added a trigger for the jump tooltip

---
+++ Assets/Scripts/TriggerActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActions : MonoBehaviour {
+
+	public GameObject tooltip;
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log (other);
+		GameObject c = Instantiate (tooltip) as GameObject;
+		c.GetComponent<ModalPanel>().Tooltip("Press Space to jump","Jump to close this tooltip",new KeyCode[]{KeyCode.Space},false);
+	}
+}
