895d2c33105e3443baa23467aefccedfc7f32798
Sephy <bob.vankamp@gmail.com>
2015-10-29 15:26:32 +0100

    Jump tooltip only appears once

--- Assets/Scripts/TriggerActions.cs
+++ Assets/Scripts/TriggerActions.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class TriggerActions : MonoBehaviour {
 
 	public GameObject tooltip;
+	private bool jumpTooltipAppeared = false;
 
 	void OnTriggerEnter(Collider other){
-		Debug.Log (other);
-		GameObject c = Instantiate (tooltip) as GameObject;
-		c.GetComponent<ModalPanel>().Tooltip("Press Space to jump","Jump to close this tooltip",new string[]{"Jump"},false);
+		if (!jumpTooltipAppeared) {
+			GameObject c = Instantiate (tooltip) as GameObject;
+			c.GetComponent<ModalPanel> ().Tooltip ("Press Space to jump", "Jump to close this tooltip", new string[]{ "Jump" }, false);
+		}
+		jumpTooltipAppeared = true;
 	}
 }
