79fb1affc187763da985b428495dbb49724fd860
Sephy <bob.vankamp@gmail.com>
2015-10-29 15:51:33 +0100

    Jump tooltip apparition now saved in playerprefs

--- Assets/Scripts/TriggerActions.cs
+++ Assets/Scripts/TriggerActions.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class TriggerActions : MonoBehaviour {
 
 	public GameObject tooltip;
 	private bool jumpTooltipAppeared = false;
 
+	void Start(){
+		jumpTooltipAppeared = Convert.ToBoolean(PlayerPrefs.GetInt ("JumpTutorial"));
+	}
+
 	void OnTriggerEnter(Collider other){
 		if (!jumpTooltipAppeared) {
 			GameObject c = Instantiate (tooltip) as GameObject;
-			c.GetComponent<ModalPanel> ().Tooltip ("Press Space to jump", "Jump to close this tooltip", new string[]{ "Jump" }, false);
+			c.GetComponent<ModalPanel> ().Tooltip ("Press the jump button to jump", "Jump to close this tooltip", new string[]{ "Jump" }, false);
+			jumpTooltipAppeared = true;
+			PlayerPrefs.SetInt ("JumpTutorial", Convert.ToInt32 (jumpTooltipAppeared));
 		}
-		jumpTooltipAppeared = true;
+
 	}
 }
