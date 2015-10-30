8c6379fa7001449fb356a010a517f9e8d2a3f053
Sephy <bob.vankamp@gmail.com>
2015-10-30 11:12:41 +0100

    Slow fade for walls

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -1,36 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WallScript : MonoBehaviour {
 
-	GameObject player;
+	public GameObject player;
 	private RaycastHit[] hits = null;
+	private float goalAlpha = 0.2f;
+	List<GameObject> oldHits;
 
-	// Use this for initialization
-	void Start () {
-		player = GameObject.Find ("ThirdPersonController");
-	}
-	
-	// Update is called once per frame
 	private void Update() {
-		if (hits != null) {
-			foreach (RaycastHit hit in hits) {
-				Renderer r = hit.collider.GetComponent<Renderer>();
-				if (r) {
-					r.material.color = Color.white;
-				}
-			}
-		}
+
 
 		Debug.DrawRay (Camera.main.transform.position, (player.transform.position - Camera.main.transform.position), Color.magenta);
 
 		hits = Physics.RaycastAll(Camera.main.transform.position, (player.transform.position - Camera.main.transform.position), Vector3.Distance(Camera.main.transform.position, player.transform.position));
-
+		if (oldHits != null) {
+			foreach (GameObject hit in oldHits) {
+				bool found = false;
+				foreach (RaycastHit newHit in hits) {
+					if (hit == newHit.collider.gameObject)
+						found = true;
+				}
+				Renderer r = hit.GetComponent<Renderer>();
+				if (r && !found) {
+					r.material.color = Color.white;
+				}
+			}
+		}
+		oldHits = new List<GameObject>();
 		foreach (RaycastHit hit in hits) {
+			oldHits.Add (hit.collider.gameObject);
 			Renderer r = hit.collider.GetComponent<Renderer>();
 			if (r && hit.collider.tag == "Wall") {
 				Color color = r.material.color;
-				color.a = 0.2f;
+				color.a = Mathf.Lerp(color.a,goalAlpha,0.8f*Time.deltaTime);
 				r.material.color = color;
 			}
 		}
