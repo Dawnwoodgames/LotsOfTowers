971dd0e006462cfd4d2652032dd09718588a1e55
Axel Schelfhout <axelschelfhout@gmail.com>
2015-10-30 15:17:14 +0100

    Add variable distance and behind height for following camera

--- Assets/Scripts/Camera/MainCameraScript.cs
+++ Assets/Scripts/Camera/MainCameraScript.cs
@@ -2,52 +2,67 @@
 using System.Collections;
 using System;
 
-public class MainCameraScript : MonoBehaviour {
-
-    GameObject centerObject;
-
-    private bool _inputLocked;
-    
-    void Start()
+namespace LotsOfTowers.Camera
+{
+    public class MainCameraScript : MonoBehaviour
     {
-        centerObject = GameObject.Find("CenterFocus");
-    }
-	
-	void Update () {
-        CameraInput();
-    }
 
-    private void CameraInput()
-    {
-        centerObject.transform.Rotate(0, Input.GetAxis("RightJoystickX") * 2, 0);
-        if (Input.GetKey("e"))
-            centerObject.transform.Rotate(Vector3.down * 2);
-        if (Input.GetKey("q"))
-            centerObject.transform.Rotate(Vector3.up * 2);
+        GameObject centerObject;
 
-        if (Input.GetKey("r"))
+        private bool _inputLocked;
+
+        public float camUpFromPlayer = 3f;
+        public float camBehindPlayer = 7f;
+
+
+        void Start()
         {
-            if (!isRotateLocked())
+            centerObject = GameObject.Find("CenterFocus");
+        }
+
+        void Update()
+        {
+            CameraInput();
+            CameraDepth();
+        }
+
+        private void CameraDepth()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, camUpFromPlayer, -camBehindPlayer);
+        }
+
+        private void CameraInput()
+        {
+            centerObject.transform.Rotate(0, Input.GetAxis("RightJoystickX") * 2, 0);
+            if (Input.GetKey("e"))
+                centerObject.transform.Rotate(Vector3.down * 2);
+            if (Input.GetKey("q"))
+                centerObject.transform.Rotate(Vector3.up * 2);
+
+            if (Input.GetKey("r"))
             {
-                centerObject.transform.Rotate(0, -90, 0);
-                LockRotate();
+                if (!isRotateLocked())
+                {
+                    centerObject.transform.Rotate(0, -90, 0);
+                    LockRotate();
+                }
             }
         }
-    }
 
-    private void LockRotate()
-    {
-        _inputLocked = true;
-        Invoke("UnlockRotate", .2f);
-    }
+        private void LockRotate()
+        {
+            _inputLocked = true;
+            Invoke("UnlockRotate", .2f);
+        }
 
-    private void UnlockRotate()
-    {
-        _inputLocked = false;
-    }
+        private void UnlockRotate()
+        {
+            _inputLocked = false;
+        }
 
-    private bool isRotateLocked()
-    {
-        return _inputLocked;
+        private bool isRotateLocked()
+        {
+            return _inputLocked;
+        }
     }
 }
