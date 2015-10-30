915df9e23989eaee6b46db81b323c631b5cf1f2d
Lars <larsh1991@hotmail.com>
2015-10-30 11:00:34 +0100

    (WIP) Camera exploration-follow

--- Assets/Scripts/Camera/CameraFollowScript.cs
+++ Assets/Scripts/Camera/CameraFollowScript.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class CameraFollowScript : MonoBehaviour {
 
     Vector3 player;
+
+    private float distancePlayerFromCamera;
 	
 	void Update () {
         player = GameObject.FindGameObjectWithTag("Player").transform.position;
         gameObject.transform.position = new Vector3(0, player.y - 2f);
+
+        distancePlayerFromCamera = Vector3.Distance(player, gameObject.transform.position);
 	}
+
+    public float GetDistancePlayerFromCamera()
+    {
+        return -distancePlayerFromCamera - 5f;
+    }
 }
--- Assets/Scripts/Camera/MainCameraScript.cs
+++ Assets/Scripts/Camera/MainCameraScript.cs
@@ -1,36 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MainCameraScript : MonoBehaviour {
 
-    GameObject main;
+    GameObject cameraObject;
 
     private bool _inputLocked;
 
 	// Use this for initialization
 	void Start ()
     {
-        main = GameObject.Find("Camera");
+        cameraObject = GameObject.Find("Camera");
     }
 	
 	// Update is called once per frame
 	void Update () {
         CameraInput();
+        CameraDepth();
 	}
+
+    private void CameraDepth()
+    {
+        gameObject.transform.position = new Vector3(cameraObject.transform.position.x, gameObject.transform.position.y, cameraObject.GetComponent<CameraFollowScript>().GetDistancePlayerFromCamera());
+    }
 
     private void CameraInput()
     {
-        main.transform.Rotate(0, Input.GetAxis("RightJoystickX") * 2, 0);
+        cameraObject.transform.Rotate(0, Input.GetAxis("RightJoystickX") * 2, 0);
         if (Input.GetKey("e"))
-            main.transform.Rotate(Vector3.down * 2);
+            cameraObject.transform.Rotate(Vector3.down * 2);
         if (Input.GetKey("q"))
-            main.transform.Rotate(Vector3.up * 2);
+            cameraObject.transform.Rotate(Vector3.up * 2);
 
         if (Input.GetKey("r"))
         {
             if (!isRotateLocked())
             {
-                main.transform.Rotate(0, -90, 0);
+                cameraObject.transform.Rotate(0, -90, 0);
                 LockRotate();
             }
         }
