a6fc2138aa285f4da8af2abf26a8ffb3a1a81636
Choraimy <Choraimy@145-24-251-100.wlan.hro.nl>
2015-11-02 12:18:24 +0100

    Added language to game manager

--- Assets/Scripts/Framework/GameManager.cs
+++ Assets/Scripts/Framework/GameManager.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace LotsOfTowers.Framework
 {
 	public class GameManager : MonoBehaviour
 	{
 		private static GameManager instance;
+		private string[] languages;
 		private Transform spawnPoint;
 
 		public static GameManager Instance
 		{
 			get { return instance; }
+		}
+
+		public string Language
+		{ // Default: en_US
+			get { return PlayerPrefs.HasKey("Language") ? PlayerPrefs.GetString("Language") : "en_US"; }
+			set { if (languages.Contains(value)) { PlayerPrefs.SetString("Language", value); } }
 		}
 
 		public Transform SpawnPoint
@@ -21,6 +29,7 @@ namespace LotsOfTowers.Framework
 		{
 			DontDestroyOnLoad(this);
 			GameManager.instance = this;
+			languages = new string[] { "en_US", "nl_NL"};
 			OnLevelWasLoaded(Application.loadedLevel);
 		}
 
