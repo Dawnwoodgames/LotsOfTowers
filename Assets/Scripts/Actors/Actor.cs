7d613c5f449c93230408d22fcf0b69c7524988ff
Choraimy <Choraimy@145-24-251-100.wlan.hro.nl>
2015-11-02 14:55:50 +0100

    Updated Actor: 	More strict checking to avoid bugs 	added GetNextOnesie and GetPreviousOnesie methods for switching through onesies

--- Assets/Scripts/Actors/Actor.cs
+++ Assets/Scripts/Actors/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using LotsOfTowers.Framework;
 using LotsOfTowers.ToolTip;
 using UnityEngine;
@@ -11,47 +12,47 @@ namespace LotsOfTowers.Actors
 		// Private fields
 		private Onesie onesie;
 		private List<Onesie> onesies;
-
+		
 		// Public fields
 		public GameObject tooltip;
-
+		
 		// Properties
 		public bool CanMoveObjects
 		{
 			get { return onesie.canMoveObjects; }
 		}
-
+		
 		public int JumpCount
 		{
 			get { return onesie.jumpCount; }
 		}
-
+		
 		public float MovementSpeed
 		{
 			get { return onesie.movementSpeed; }
 		}
-
+		
 		public Onesie[] Onesies
 		{
 			get { return onesies.ToArray(); }
 		}
-
+		
 		// Methods
 		public void AddOnesie(Onesie onesie)
 		{
-			if (!onesies.Contains(onesie))
+			if (onesies.Where(o => o.name == onesie.name).Count() == 0)
 			{
 				onesies.Add(onesie);
 			}
 		}
-
+		
 		public void Awake()
 		{
 			DontDestroyOnLoad(gameObject);
 			this.onesie = Onesie.Load("Default");
 			this.onesies = new List<Onesie>(new Onesie[] { onesie });
 		}
-
+		
 		public void Equip(Onesie onesie)
 		{
 			if (onesies.Contains(onesie))
@@ -59,7 +60,27 @@ namespace LotsOfTowers.Actors
 				this.onesie = onesie;
 			}
 		}
-
+		
+		public Onesie GetNextOnesie(Onesie current) {
+			int index = onesies.IndexOf(current);
+			
+			if (index != -1 && index != onesies.Count) {
+				return onesies.ElementAt(index + 1);
+			}
+			
+			return onesies.First();
+		}
+		
+		public Onesie GetPreviousOnesie(Onesie current) {
+			int index = onesies.IndexOf(current);
+			
+			if (index > 0) {
+				return onesies.ElementAt(index - 1);
+			}
+			
+			return onesies.Last();
+		}
+		
 		public void Start()
 		{
 			Tooltip.ShowTooltip(tooltip, "Movement", false, new string[] { "Horizontal", "Vertical" });
