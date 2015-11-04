43296cef43acac4e813bff1ba8694acbc5caa464
Tim Ververs <timververs@msn.com>
2015-11-04 15:34:01 +0100

    Refactoring + cleaning root map

--- Assets/Scripts/Extensions/Extensions.cs
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -1,13 +1,16 @@
 ﻿using SmartLocalization;
 
-namespace LotsOfTowers {
+namespace LotsOfTowers
+{
 	// This class is in the root namespace on purpose!
 	// Do not change
-	public static class Extensions {
+	public static class Extensions
+	{
 		// Usage:
 		// string s = "tooltips.jump.title";
 		// s.Localize();
-		public static string Localize(this string str) {
+		public static string Localize(this string str)
+		{
 			return LanguageManager.Instance.GetTextValue(str);
 		}
 	}
--- Assets/Scripts/ToolTip/Tooltip.cs
+++ Assets/Scripts/ToolTip/Tooltip.cs
@@ -6,7 +6,6 @@ namespace LotsOfTowers.ToolTip
 {
 	public class Tooltip : MonoBehaviour
 	{
-		// Create constants instead please :)
 		private static Dictionary<string, string[]> tooltips = new Dictionary<string, string[]>() {
 		{ "Jump", new string[]{ "Press the Jump button to Jump", "Close this dialog by jumping" } },
 		{ "Movement", new string[]{ "Press the arrow keys, wasd or use a analog stick to move", "Try moving a little" } },
@@ -18,8 +17,8 @@ namespace LotsOfTowers.ToolTip
 			if (!Convert.ToBoolean(PlayerPrefs.GetInt("Tutorial_" + name)))
 			{
 				GameObject c = Instantiate(tooltip) as GameObject;
-				String hint = ("tooltip." + name.ToLower() + ".hint").Localize();
-				String title = ("tooltip." + name.ToLower() + ".title").Localize();
+				string hint = ("tooltip." + name.ToLower() + ".hint").Localize();
+				string title = ("tooltip." + name.ToLower() + ".title").Localize();
 
 				c.GetComponent<ModalPanel>().Tooltip(title, hint, possibleCloseKeys, autoClose);
 				PlayerPrefs.SetInt("Tutorial_" + name, Convert.ToInt32(true));
