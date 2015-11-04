e8c1084673d7d9b48306518c74b7db9675ce6d98
Sephy <bob.vankamp@gmail.com>
2015-11-04 13:05:14 +0100

    Show a tooltip when you come near the chicken (cube)

---
+++ Assets/Scripts/Triggers/ChickenTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickenTrigger : MonoBehaviour {
+
+    public GameObject tooltipCanvas;
+    private bool alreadyShown = false;
+
+	void Start () {
+        tooltipCanvas.SetActive(false);
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        if (!alreadyShown && collider.tag == "Player")
+        {
+            tooltipCanvas.SetActive(true);
+            alreadyShown = true;
+        }
+
+    }
+}
