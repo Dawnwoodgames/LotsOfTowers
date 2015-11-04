ebbd755eeabbd7cbbe6a49c3b2a4a9db6c55f258
Choraimy <Choraimy@MacBook-Pro-van-Choraimy.local>
2015-11-04 12:47:29 +0100

    Wrote translate script, deleted unused assets

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using LotsOfTowers.Framework;
+
+namespace LotsOfTowers {
+	// This class is in the root namespace on purpose!
+	// Do not change
+	public static class Extensions {
+
+		// Usage:
+		// string s = "tooltips.jump.title";
+		// s.Localize();
+		public static string Localize(this string str, string key) {
+			if (GameManager.Instance.Language == "en_US") {
+				return Assets.Localization.en_US.ResourceManager.GetString (key);
+			} else if (GameManager.Instance.Language == "nl_NL") {
+				// TODO: create nl_NL.resx and add it here
+			}
+		}
+	}
+}
