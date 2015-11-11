1c17a30f7098bbbfc022e241dc1afa84cda3b677
Choraimy <Choraimy@145-24-235-96.wlan.hro.nl>
2015-11-11 14:16:10 +0100

    MovableObjects see their return

---
+++ Assets/Scripts/Platform/MovableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using LotsOfTowers.Actors;
+
+namespace LotsOfTowers.Platform
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public sealed class MovableObject : MonoBehaviour
+	{
+		private Player player;
+		private Rigidbody rigidBody;
+		
+		public void Awake()
+		{
+			player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+			rigidBody = GetComponent<Rigidbody>();
+			rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+		}
+		
+		public void Update()
+		{
+			rigidBody.isKinematic = !player.CanMoveObjects;
+		}
+	}
+}
