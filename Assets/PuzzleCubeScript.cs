8af24a2cd3900d3e3cfb498a28a66612d0d1bed8
Lars <larsh1991@hotmail.com>
2015-11-12 15:44:02 +0100

    Shatter cube implemented

---
+++ Assets/PuzzleCubeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleCubeScript : MonoBehaviour {
+
+    private Transform breakEvent;
+
+	void Start () {
+        breakEvent = GameObject.Find("BreakEvent").transform;
+	}
+	
+	void Update () {
+        breakEvent.GetComponent<Rigidbody>().AddForce(transform.up * 20);
+	}
+}
