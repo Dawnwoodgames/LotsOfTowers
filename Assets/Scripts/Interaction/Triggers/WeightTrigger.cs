e7ccaa256d0e38837044029b6ff65ba8b6a2815c
Choraimy <Choraimy@MacBook-Pro-van-Choraimy.local>
2015-11-12 13:11:03 +0100

    Added WeightTrigger that will fall down when triggered (works the same way as slide trigger)

---
+++ Assets/Scripts/Interaction/Triggers/WeightTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace LotsOfTowers.Interaction.Triggers {
+	[RequireComponent(typeof(Rigidbody))]
+	public sealed class WeightTrigger : TriggerBehaviour {
+		private List<Rigidbody> objects;
+		private Rigidbody rigidBody;
+
+		public float MassThreshold; // Max mass this object can carry without falling
+
+		public float CarryWeight { get { return objects.Select(g => g.mass).Sum(); } }
+
+		public void Awake() {
+			this.objects = new List<Rigidbody>();
+			this.rigidBody = GetComponent<Rigidbody>();
+			this.rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+			this.rigidBody.isKinematic = true;
+		}
+
+		public override IEnumerator TriggerOn(GameObject source) {
+			Rigidbody body = source.GetComponent<Rigidbody>();
+
+			if (body != null) {
+				objects.Add(body);
+			}
+
+			if (CarryWeight >= MassThreshold) {
+				rigidBody.isKinematic = false;
+			}
+
+			yield return null;
+		}
+
+		public override IEnumerator TriggerOff(GameObject source) {
+			Rigidbody body = source.GetComponent<Rigidbody>();
+			
+			if (body != null) {
+				objects.Remove(body);
+			}
+
+			if (CarryWeight < MassThreshold) {
+				rigidBody.isKinematic = true;
+			}
+
+			yield return null;
+		}
+	}
+}
