f166045a1fd1bfe79024a0f3120a0b0f52fa496e
Choraimy <Choraimy@MacBook-Pro-van-Choraimy.local>
2015-11-12 15:21:40 +0100

    Removed side-detection, because it does not work with MeshColliders

--- Assets/Scripts/Interaction/PressurePlate.cs
+++ Assets/Scripts/Interaction/PressurePlate.cs
@@ -10,23 +10,8 @@ namespace LotsOfTowers.Interaction {
 			if (colliding) return;
 			colliding = true;
 
-			bool trigger;
-
-			if (Side == Side.Bottom || Side == Side.Top) {
-				float dY = collision.gameObject.GetComponent<Collider>().bounds.extents.y - GetComponent<Collider>().bounds.extents.y;
-				trigger = (dY < 0 && Side == Side.Top) || (dY > 0 && Side == Side.Bottom);
-			} else if (Side == Side.Left || Side == Side.Right) {
-				float dX = collision.gameObject.GetComponent<Collider>().bounds.extents.x - GetComponent<Collider>().bounds.extents.x;
-				trigger = (dX < 0 && Side == Side.Right) || (dX > 0 && Side == Side.Left);
-			} else {
-				float dZ = collision.gameObject.GetComponent<Collider>().bounds.extents.z - GetComponent<Collider>().bounds.extents.z;
-				trigger = (dZ < 0 && Side == Side.Front) || (dZ > 0 && Side == Side.Behind);
-			}
-
-			if (trigger) {
-				foreach (TriggerBehaviour triggerBehaviour in GetComponents<TriggerBehaviour>()) {
-					StartCoroutine(triggerBehaviour.TriggerOn(collision.gameObject));
-				}
+			foreach (TriggerBehaviour triggerBehaviour in GetComponents<TriggerBehaviour>()) {
+				StartCoroutine(triggerBehaviour.TriggerOn (collision.gameObject));
 			}
 		}
 
@@ -34,39 +19,13 @@ namespace LotsOfTowers.Interaction {
 			if (colliding) return;
 			colliding = true;
 
-			bool trigger;
-			
-			if (Side == Side.Bottom || Side == Side.Top) {
-				float dY = collision.gameObject.GetComponent<Collider>().bounds.extents.y - GetComponent<Collider>().bounds.extents.y;
-				trigger = (dY < 0 && Side == Side.Top) || (dY > 0 && Side == Side.Bottom);
-			} else if (Side == Side.Left || Side == Side.Right) {
-				float dX = collision.gameObject.GetComponent<Collider>().bounds.extents.x - GetComponent<Collider>().bounds.extents.x;
-				trigger = (dX < 0 && Side == Side.Right) || (dX > 0 && Side == Side.Left);
-			} else {
-				float dZ = collision.gameObject.GetComponent<Collider>().bounds.extents.z - GetComponent<Collider>().bounds.extents.z;
-				trigger = (dZ < 0 && Side == Side.Front) || (dZ > 0 && Side == Side.Behind);
-			}
-			
-			if (trigger) {
-				foreach (TriggerBehaviour triggerBehaviour in GetComponents<TriggerBehaviour>()) {
-					StartCoroutine(triggerBehaviour.TriggerOff(collision.gameObject));
-				}
+			foreach (TriggerBehaviour triggerBehaviour in GetComponents<TriggerBehaviour>()) {
+				StartCoroutine(triggerBehaviour.TriggerOff(collision.gameObject));
 			}
 		}
 
 		public void Update() {
 			colliding = false;
 		}
-	}
-	
-	public enum Side {
-		Top,
-		Bottom,
-
-		Left,
-		Right,
-
-		Front,
-		Behind
 	}
 }
