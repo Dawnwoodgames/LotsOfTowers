77cf2617e7ba41b22753061477bda6719a8d2760
Lars <larsh1991@hotmail.com>
2015-11-17 15:03:14 +0100

    Brick puzzle complete

--- Assets/Scripts/Interaction/PuzzleCube.cs
+++ Assets/Scripts/Interaction/PuzzleCube.cs
@@ -1,59 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace LotsOfTowers.Interaction
 {
-	public class PuzzleCube : MonoBehaviour
-	{
+    public class PuzzleCube : MonoBehaviour
+    {
         public GameObject snapArea;
         private Transform floor;
-		private Transform sphere;
-		private List<Transform> fractures = new List<Transform>();
+        private Transform sphere;
+        private List<Transform> fractures = new List<Transform>();
 
-		void Start()
-		{
-			//sphere = GameObject.Find("Sphere").transform;
-			floor = GameObject.Find("FloatingFloor").transform;
-			foreach (Transform child in floor)
-			{
-				fractures.Add(child);
-			}
-		}
+        void Start()
+        {
+            floor = GameObject.Find("FloatingFloor").transform;
+            foreach (Transform child in floor)
+            {
+                fractures.Add(child);
+            }
+        }
 
-		void FixedUpdate()
-		{
-			if (Input.GetKeyDown(KeyCode.C))
-			{
-				//sphere.GetComponent<Rigidbody>().AddForce(transform.up * 1200, ForceMode.Force);
-				foreach (Transform child in fractures)
-				{
-                    child.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                foreach (Transform child in fractures)
+                {
+                    child.localScale = new Vector3(0.45f, 0.45f, 0.45f);
                     child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
                     child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
                     child.GetComponent<Rigidbody>().useGravity = true;
                 }
-            }
-			if (Input.GetKeyDown(KeyCode.V))
-			{
-                snapArea.SetActive(true);
-				foreach (Transform child in fractures)
-				{
-					child.GetComponent<Rigidbody>().mass = 0.1f;
-					child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-				}
+                Destroy(GetComponent<BoxCollider>());
 
-				//Destroy(sphere.gameObject);
-			}
-		}
+                StartCoroutine(ActivateFrozenFractures());
+            }
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                snapArea.SetActive(true);
+                foreach (Transform child in fractures)
+                {
+                    child.GetComponent<Rigidbody>().mass = 0.1f;
+                    child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+                }
+            }
+        }
 
         void OnTriggerEnter(Collider coll)
         {
-            foreach (Transform child in fractures)
+            if (coll.tag == "Player")
             {
-                child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
-                child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
-                child.GetComponent<Rigidbody>().useGravity = true;
+                foreach (Transform child in fractures)
+                {
+                    child.localScale = new Vector3(0.45f, 0.45f, 0.45f);
+                    child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
+                    child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
+                    child.GetComponent<Rigidbody>().useGravity = true;
+                }
+                Destroy(GetComponent<BoxCollider>());
+
+                StartCoroutine(ActivateFrozenFractures());
             }
         }
+
+        private IEnumerator ActivateFrozenFractures()
+        {
+            yield return new WaitForSeconds(1);
+
+            snapArea.SetActive(true);
+            foreach (Transform child in fractures)
+            {
+                child.GetComponent<Rigidbody>().mass = 0.1f;
+                child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            }
         }
+    }
 }
