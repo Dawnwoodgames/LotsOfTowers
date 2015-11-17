9a7eaf8c2b162045e3cd38c8e85824cc73363dcf
Tim Ververs <timververs@msn.com>
2015-11-17 16:04:39 +0100

    LARS EN IK ZIJN BAAS #NOHOMO

--- Assets/Scripts/Interaction/PuzzleCube.cs
+++ Assets/Scripts/Interaction/PuzzleCube.cs
@@ -7,42 +7,15 @@ namespace LotsOfTowers.Interaction
     public class PuzzleCube : MonoBehaviour
     {
         public GameObject snapArea;
-        private Transform floor;
         private Transform sphere;
         private List<Transform> fractures = new List<Transform>();
 
         void Start()
         {
-            floor = GameObject.Find("FloatingFloor").transform;
-            foreach (Transform child in floor)
+            foreach (Transform child in transform)
             {
+				child.GetComponent<Rigidbody>().isKinematic = true;
                 fractures.Add(child);
-            }
-        }
-
-        void FixedUpdate()
-        {
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                foreach (Transform child in fractures)
-                {
-                    child.localScale = new Vector3(0.45f, 0.45f, 0.45f);
-                    child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
-                    child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
-                    child.GetComponent<Rigidbody>().useGravity = true;
-                }
-                Destroy(GetComponent<BoxCollider>());
-
-                StartCoroutine(ActivateFrozenFractures());
-            }
-            if (Input.GetKeyDown(KeyCode.V))
-            {
-                snapArea.SetActive(true);
-                foreach (Transform child in fractures)
-                {
-                    child.GetComponent<Rigidbody>().mass = 0.1f;
-                    child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-                }
             }
         }
 
@@ -50,14 +23,21 @@ namespace LotsOfTowers.Interaction
         {
             if (coll.tag == "Player")
             {
-                foreach (Transform child in fractures)
+				foreach (Transform child in fractures)
                 {
+					Rigidbody childRigid = child.GetComponent<Rigidbody>();
+
+					childRigid.isKinematic = false;
                     child.localScale = new Vector3(0.45f, 0.45f, 0.45f);
-                    child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
-                    child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
-                    child.GetComponent<Rigidbody>().useGravity = true;
-                }
-                Destroy(GetComponent<BoxCollider>());
+
+					childRigid.AddForce(transform.up * 923, ForceMode.Force);
+					childRigid.AddForce(child.forward * 291, ForceMode.Force);
+
+					childRigid.useGravity = true;
+				}
+
+				//Disable trigger to add force to fracments
+				GetComponent<BoxCollider>().enabled = false;
 
                 StartCoroutine(ActivateFrozenFractures());
             }
@@ -65,13 +45,12 @@ namespace LotsOfTowers.Interaction
 
         private IEnumerator ActivateFrozenFractures()
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(2);
 
             snapArea.SetActive(true);
             foreach (Transform child in fractures)
             {
                 child.GetComponent<Rigidbody>().mass = 0.1f;
-                child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
             }
         }
     }
--- Assets/Scripts/Interaction/Triggers/FractureSnapTrigger.cs
+++ Assets/Scripts/Interaction/Triggers/FractureSnapTrigger.cs
@@ -4,15 +4,13 @@ namespace LotsOfTowers.Interaction.Triggers
 {
 	public class FractureSnapTrigger : MonoBehaviour
 	{
-		public GameObject fracture;
-
 		void OnTriggerEnter(Collider coll)
 		{
 			if (coll.tag == "Fracture")
 			{
-				fracture.transform.position = gameObject.transform.position;
-				fracture.transform.rotation = new Quaternion(0, 0, 0, 0);
-				fracture.GetComponent<Rigidbody>().isKinematic = true;
+				coll.transform.position = gameObject.transform.position;
+				coll.transform.rotation = new Quaternion(0, 0, 0, 0);
+				coll.GetComponent<Rigidbody>().isKinematic = true;
 			}
 		}
 	}
