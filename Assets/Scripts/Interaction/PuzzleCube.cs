ddfe58ac5c22017559718bdd2fbae8f884d7c015
Lars <larsh1991@hotmail.com>
2015-11-17 14:24:37 +0100

    Floor trigger implemented

--- Assets/Scripts/Interaction/PuzzleCube.cs
+++ Assets/Scripts/Interaction/PuzzleCube.cs
@@ -5,7 +5,8 @@ namespace LotsOfTowers.Interaction
 {
 	public class PuzzleCube : MonoBehaviour
 	{
-		private Transform floor;
+        public GameObject snapArea;
+        private Transform floor;
 		private Transform sphere;
 		private List<Transform> fractures = new List<Transform>();
 
@@ -26,19 +27,33 @@ namespace LotsOfTowers.Interaction
 				//sphere.GetComponent<Rigidbody>().AddForce(transform.up * 1200, ForceMode.Force);
 				foreach (Transform child in fractures)
 				{
-					//child.GetComponent<Rigidbody>().useGravity = true;
-				}
-			}
+                    child.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                    child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
+                    child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
+                    child.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
 			if (Input.GetKeyDown(KeyCode.V))
 			{
+                snapArea.SetActive(true);
 				foreach (Transform child in fractures)
 				{
 					child.GetComponent<Rigidbody>().mass = 0.1f;
 					child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 				}
 
-				Destroy(sphere.gameObject);
+				//Destroy(sphere.gameObject);
 			}
 		}
-	}
+
+        void OnTriggerEnter(Collider coll)
+        {
+            foreach (Transform child in fractures)
+            {
+                child.GetComponent<Rigidbody>().AddForce(transform.up * 800, ForceMode.Force);
+                child.GetComponent<Rigidbody>().AddForce(child.forward * 120, ForceMode.Force);
+                child.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+        }
 }
