f9bc308f06b1bf3174bd84fed43f0783dd956963
Lars <larsh1991@hotmail.com>
2015-11-19 14:09:01 +0100

    Elephant launches player

---
+++ Assets/Scripts/Interaction/Triggers/ElephantTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+    public class ElephantTrigger : MonoBehaviour
+    {
+        public GameObject libraTrigger;
+        public GameObject elephantTrigger;
+        public GameObject elephantLaunch;
+        private LibraTrigger triggerScript;
+        private NavMeshAgent agent;
+
+        private bool agentActive = false;
+        private bool elephantStrafes = false;
+
+        void Start()
+        {
+            triggerScript = libraTrigger.GetComponent<LibraTrigger>();
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (triggerScript.firstTrigger)
+                MoveElephant();
+
+            if (agentActive)
+                agent.SetDestination(elephantLaunch.transform.position);
+
+            if (triggerScript.secondTrigger)
+                MoveElephant();
+        }
+
+        private void MoveElephant() {
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.name == "elephanttrigger")
+            {
+                agentActive = true;
+                this.transform.Rotate(new Vector3(.45f, 0f, 0f));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/LibraTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+    public class LibraTrigger : MonoBehaviour
+    {
+
+        public GameObject libra;
+        public bool firstTrigger = false;
+        public bool secondTrigger = false;
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player" && libra.transform.rotation.x >= -13 && !firstTrigger)
+            {
+                firstTrigger = true;
+                this.transform.localPosition = new Vector3(-10.49f, -5f, 1f);
+            }
+            if (coll.tag == "Player" && libra.transform.rotation.x >= 13 && firstTrigger)
+            {
+                secondTrigger = true;
+            }
+        }
+    }
+}
