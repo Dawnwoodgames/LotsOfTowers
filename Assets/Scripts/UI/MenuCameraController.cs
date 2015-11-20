babfd99a5d3dc353027f3466b67f32b8f9f048ed
Choraimy <Choraimy@145-24-227-70.wlan.hro.nl>
2015-11-20 10:06:28 +0100

    Added new 3D menu with main menu and preferences menu, along with 1st version of menu transitions

---
+++ Assets/Scripts/UI/MenuCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace LotsOfTowers.UI {
+	[RequireComponent(typeof(Camera))]
+	public sealed class MenuCameraController : MonoBehaviour {
+		private Transform mount;
+
+		public float speed = 0.1f;
+		public float zoom = 1;
+
+		public void Awake() {
+			SetActiveMenu(FindObjectOfType<Canvas>().gameObject);
+		}
+
+		public void SetActiveMenu(GameObject menu) {
+			try {
+				mount = GameObject.Find("Menu/" + menu.name + "/Mounting Point").transform;
+			} catch (Exception) { }
+		}
+
+		public void Update() {
+			if (mount != null) {
+				transform.position = Vector3.Lerp(transform.position, mount.position, speed);
+				transform.rotation = Quaternion.Slerp(transform.rotation, mount.rotation, speed);
+			}
+		}
+	}
+}
