66e23f1d9c8a35dbee15a04db329d5e153f25007
Choraimy <Choraimy@145-24-251-45.wlan.hro.nl>
2015-11-24 12:24:14 +0100

    Moved functionality into the right scripts

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 namespace LotsOfTowers.UI {
 	[RequireComponent(typeof(Camera))]
 	public sealed class CameraController : MonoBehaviour {
-		private Transform mount;
 
-		public float speed = 0.1f;
-
-		public void Awake() {
-			SetActiveMenu(GameObject.Find("Menu/Main"));
-		}
-
-		public void SetActiveMenu(GameObject menu) {
-			try {
-				mount = GameObject.Find("Menu/" + menu.name + "/Mounting Point").transform;
-			} catch (Exception) { }
-		}
+		public Transform mount;
+		public float speed;
 
 		public void Update() {
-			if (mount != null) {
+			if (mount != null && speed > 0) {
 				transform.position = Vector3.Lerp(transform.position, mount.position, speed);
 				transform.rotation = Quaternion.Slerp(transform.rotation, mount.rotation, speed);
 			}
--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,19 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using SmartLocalization;
 
 namespace LotsOfTowers.UI {
 	public sealed class MenuController : MonoBehaviour {
+		private CameraController camera;
+		private EventSystem eventSystem;
 		private Text[] labels;
+		private GameObject[] menus;
+		
+		public ColorBlock colors; // Colors used for all UI elements in this controller
+		public Font font; // Font used for all text elements in this controller
 
 		public void Awake() {
+			this.camera = FindObjectOfType<CameraController>();
+			this.eventSystem = FindObjectOfType<EventSystem>();
+			this.font = (font == null) ? Resources.GetBuiltinResource<Font>("Arial.ttf") : font;
 			this.labels = GetComponentsInChildren<Text>();
+			this.menus = GetComponentsInChildren<Canvas>().Select(c => c.gameObject).ToArray();
+
+			foreach (Text label in labels) {
+				label.font = font;
+				label.text = label.text.Localize();
+			}
+		}
+
+		public void SetActiveMenu(GameObject menu) {
+			if (menus.Contains(menu)) {
+				camera.mount = GameObject.Find(name + "/" + menu.name + "/Mounting Point").transform;
+			}
 		}
 
 		public void Start() {
-			foreach (Text label in labels) {
-				label.text = label.text.Localize();
-			}
+			SetActiveMenu(menus.FirstOrDefault());
+
+			Button b = GetComponentInChildren<Button> ();
+			Debug.Log ("normal: " + b.colors.normalColor);
+			Debug.Log ("highlight: " + b.colors.highlightedColor);
+			Debug.Log ("pressed: " + b.colors.pressedColor);
 		}
 
 		// Event handles used by the menu
