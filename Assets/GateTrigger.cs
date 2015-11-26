b05f3d483e12b4d9f48b1ed0d7101f53148d4044
Tim Ververs <timververs@msn.com>
2015-11-26 18:53:50 +0100

    EVERYTHINGGGGG

---
+++ Assets/GateTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using LotsOfTowers.Actors;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+	public class GateTrigger : MonoBehaviour
+	{
+		public GameObject DownwardsObject; // Object that will be moved downwards
+		private bool inTrigger = false;
+		private Player player;
+		
+		void Update()
+		{
+			if (DownwardsObject.transform.rotation.z != 0f)
+			{
+				DownwardsObject.transform.rotation = Quaternion.Slerp(DownwardsObject.transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * 1);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Interaction/Triggers/CombineFractures.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+	public class CombineFractures : MonoBehaviour
+	{
+		public FanSnapTrigger[] fracturesSnapped;
+
+		void Start()
+		{
+			try
+			{
+				fracturesSnapped = GetComponentsInChildren<FanSnapTrigger>();
+			}
+			catch (System.Exception)
+			{
+
+				throw;
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if(fracturesSnapped.All(f => f.isPlaced == true))
+			{
+
+			}
+		}
+	}
+}
