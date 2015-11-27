135a4a8c03796ee4cdbf3cb0aa644786f9e4db95
Choraimy <Choraimy@145-24-251-45.wlan.hro.nl>
2015-11-27 13:50:20 +0100

    Added GateTriggers, removed shadows to reduce performance impact

--- Assets/Scripts/Interaction/Triggers/BuoyGateTrigger.cs
+++ Assets/Scripts/Interaction/Triggers/BuoyGateTrigger.cs
@@ -1,11 +1,30 @@
-﻿using UnityEngine;
+﻿using LotsOfTowers.Environment;
+using UnityEngine;
 
 namespace LotsOfTowers.Interaction.Trigger {
+	[RequireComponent(typeof(Collider))]
 	public sealed class BuoyGateTrigger : MonoBehaviour {
+		private Buoy buoyLeft, buoyRight;
+		private bool triggered;
 
-		public void Update() {
-			Debug.DrawRay (transform.position, Vector3.left);
-			Debug.DrawRay (transform.position, Vector3.right);
+		public void Awake() {
+			RaycastHit hit;
+			
+			if (Physics.Raycast(transform.position, Vector3.left, out hit)) {
+				buoyLeft = hit.collider.GetComponent<Buoy>();
+			}
+
+			if (Physics.Raycast(transform.position, Vector3.right, out hit)) {
+				buoyRight = hit.collider.GetComponent<Buoy>();
+			}
+		}
+
+		public void OnTriggerEnter() {
+			if (triggered || buoyLeft.Red || buoyRight.Red) {
+				// not allowed
+			}
+
+			triggered = true;
 		}
 	}
 }
