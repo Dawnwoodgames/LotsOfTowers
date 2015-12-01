ec1edaaf47fadfd1c6673050fc1dc950f80ae2ec
Tim Ververs <timververs@msn.com>
2015-12-01 18:42:46 +0100

    Refactoring + slidetriggers in Tower 2 aanpassen

---
+++ Assets/Scripts/Interaction/Triggers/FractureReset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+	public class FractureReset : MonoBehaviour
+	{
+		public Vector3 resetPosition = new Vector3();
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (transform.position.y > -1f)
+			{
+				ResetPosition();
+			}
+		}
+
+		private void ResetPosition()
+		{
+			transform.position = resetPosition;
+        }
+	}
+}
