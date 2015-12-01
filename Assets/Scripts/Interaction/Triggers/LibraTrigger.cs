2594285df5cf7427b10fdd293d6fde3f92aceaca
Sephy <bob.vankamp@gmail.com>
2015-12-01 11:07:42 +0100

    Started fixing the Libra

--- Assets/Scripts/Interaction/Triggers/LibraTrigger.cs
+++ Assets/Scripts/Interaction/Triggers/LibraTrigger.cs
@@ -6,27 +6,22 @@ namespace LotsOfTowers.Interaction.Triggers
     public class LibraTrigger : MonoBehaviour
     {
         public GameObject libra;
-        private GameObject elephant;
+        public GameObject elephant;
         public bool playerOnLibra = false;
         public bool elephantReadyToLaunch = false;
 
         private void Start()
         {
-            elephant = GameObject.Find("NpcElephant");
+            
         }
 
         private void OnTriggerEnter(Collider coll)
         {
-            if (coll.tag == "Player" && libra.transform.rotation.x >= -13 && !playerOnLibra)
+            if (coll.tag == "Player")
             {
                 playerOnLibra = true;
-                this.transform.localPosition = new Vector3(-19f, transform.localPosition.y, transform.localPosition.z);
+                coll.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
                 libra.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            }
-            if (coll.tag == "Player" && elephantReadyToLaunch && playerOnLibra)
-            {
-                Destroy(elephant.GetComponent<NavMeshAgent>());
-                elephant.GetComponent<Rigidbody>().useGravity = true;
             }
         }
     }
