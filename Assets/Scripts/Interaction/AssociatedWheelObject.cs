e34bbc734336fbd0551e82edaac0ea98fb57488f
Lars <larsh1991@hotmail.com>
2015-12-02 14:16:32 +0100

    cypher puzzle done

---
+++ Assets/Scripts/Interaction/AssociatedWheelObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction
+{
+    public class AssociatedWheelObject : MonoBehaviour
+    {
+        public GameObject floor;
+        public GameObject[] cypherWheel;
+        public int rotateAmount;
+
+        private HamsterWheel wheel;
+        private bool isSpinning = false;
+        
+        void Start()
+        {
+            wheel = GetComponent<HamsterWheel>();
+        }
+
+        void Update()
+        {
+            if (wheel.GetRotateSpeed() >= 18)
+                RotateObject();
+        }
+
+        private void RotateObject()
+        {
+            if (!isSpinning)
+            {
+                if (this.gameObject.tag == "Fracture")
+                {
+                    isSpinning = true;
+                    floor.transform.Rotate(Vector3.forward * 15 * Time.deltaTime);
+                } else {
+                    isSpinning = true;
+                    foreach (GameObject cWheel in cypherWheel)
+                        cWheel.transform.Rotate(Vector3.left * 90 * rotateAmount);
+                    StartCoroutine(DelaySpin());
+                }
+            }
+        }
+
+        private IEnumerator DelaySpin()
+        {
+            yield return new WaitForSeconds(1);
+            isSpinning = false;
+        }
+    }
+}
---
+++ Assets/WaterInteraction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterInteraction : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if (!coll.GetComponent<LotsOfTowers.Actors.Player>().IsElephant)
+            coll.GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Impulse);
+    }
+}
