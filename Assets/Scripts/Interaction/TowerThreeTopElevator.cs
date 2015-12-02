e303713fe8b8f418d34683d03badc153dfb05c44
Axel Schelfhout <axelschelfhout@gmail.com>
2015-12-02 15:41:10 +0100

    Carpet to top, aint working like it should. Cho is gonna fix tis now cuz i aint a real porgrammer

--- Assets/Scripts/Interaction/TowerThreeTopElevator.cs
+++ Assets/Scripts/Interaction/TowerThreeTopElevator.cs
@@ -7,7 +7,12 @@ namespace LotsOfTowers.Interaction
     public class TowerThreeTopElevator : MonoBehaviour {
 
         public float targetY;
+        public float targetZ;
+        public float halfwayZ;
         public float elevateSpeed = 1.0F;
+        
+        public GameObject invisWalls;
+        public GameObject invisWallEntrance;
 
         private float elevatorY;
         private float elevatorX;
@@ -15,48 +20,128 @@ namespace LotsOfTowers.Interaction
 
         private bool elevating = false;
         private bool elevated = false;
+        private bool halfwayelevated = false;
+        private bool secondwayelevated = false;
 
         private PlayerController playerController;
 
         private Vector3 targetPosition;
+        private Vector3 targetHalfwayPosition;
+        private Vector3 targetSecondPosition;
 
         private float startTime;
         private float journeyLength;
+        private float journeyHalfLenght;
+        private float journeySecondLenght;
+        private float halfofdist;
 
         void Start()
         {
-            elevatorX = transform.position.x;
-            elevatorY = transform.position.y;
-            elevatorZ = transform.position.z;
+            if (targetZ != 0)
+            {
+                elevatorZ = targetZ;
+            }
+            else
+            {
+                elevatorZ = transform.localPosition.z;
+            }
+
+            elevatorY = transform.localPosition.y;
+            elevatorX = transform.localPosition.x;
             playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 
+
+            halfofdist = (transform.localPosition.y - targetY) / 2;
+            halfofdist = transform.localPosition.y - halfofdist;
+
+            targetHalfwayPosition = new Vector3(elevatorX, halfofdist, halfwayZ);
+            targetSecondPosition = new Vector3(elevatorX, targetY, halfwayZ);
             targetPosition = new Vector3(elevatorX, targetY, elevatorZ);
 
-            journeyLength = Vector3.Distance(transform.position, targetPosition);
+            journeyHalfLenght = Vector3.Distance(transform.localPosition, targetHalfwayPosition);
+            journeySecondLenght = Vector3.Distance(transform.localPosition, targetSecondPosition);
+            journeyLength = Vector3.Distance(transform.localPosition, targetPosition);
+            
         }
 
-        void Update()
+        void FixedUpdate()
         {
             if (elevating)
             {
-                if (transform.position == targetPosition)
-                {
-                    elevating = false;
-                    elevated = true;
-                }
-                else
-                {
-                    float distCovered = (Time.time - startTime) * elevateSpeed;
-                    float fracJourney = distCovered / journeyLength;
-                    transform.position = Vector3.Lerp(transform.position, targetPosition, fracJourney);
-                }
+                elevateHalfway();
             }
 
-            if(elevated)
+            if (halfwayelevated)
             {
+                elevateSecond();
+            }
+
+            if (secondwayelevated)
+            {
+                elevateToTop();
+            }
+            
+            if (elevated)
+            {
+                elevating = false;
+                halfwayelevated = false;
+                secondwayelevated = false;
+
                 playerController.enabled = true;
+                invisWallEntrance.SetActive(false);
             }
         }
+
+        void elevateSecond()
+        {
+            if (transform.localPosition == targetSecondPosition)
+            {
+                startTime = Time.time;
+                secondwayelevated = true;
+                halfwayelevated = false;
+            }
+            else
+            {
+                float distCovered = (Time.time - startTime) * 0.25f;
+                float fracJourney = distCovered / journeySecondLenght;
+
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetSecondPosition, fracJourney);
+            }
+        }
+
+        void elevateHalfway()
+        {
+            if (transform.localPosition == targetHalfwayPosition)
+            {
+                startTime = Time.time;
+                elevating = false;
+                halfwayelevated = true;
+            }
+            else
+            {
+                float distCovered = (Time.time - startTime) * (elevateSpeed / 2);
+                float fracJourney = distCovered / journeyHalfLenght;
+
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetHalfwayPosition, fracJourney);
+            }
+        }
+
+        void elevateToTop()
+        {
+            if(Vector3.Distance(transform.localPosition, targetPosition) < 0.1f)
+            {
+                secondwayelevated = false;
+                elevated = true;
+            }
+            else
+            {
+                float distCovered = (Time.time - startTime) * (elevateSpeed / 2);
+                float fracJourney = distCovered / journeyLength;
+
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, fracJourney);
+            }
+        }
+
 
         void OnTriggerEnter()
         {
@@ -65,6 +150,7 @@ namespace LotsOfTowers.Interaction
                 startTime = Time.time;
                 playerController.enabled = false;
                 elevating = true;
+                invisWalls.SetActive(true);
             }
         
         }
