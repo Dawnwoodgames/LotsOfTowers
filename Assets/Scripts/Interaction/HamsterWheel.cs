eaf45b976cab5f867f6584a4c088597c4dea3941
tislars <contact@larshoevenaar.nl>
2015-12-08 12:31:14 +0100

    Hamsterwheel updated for waterhole puzzle

---
+++ Assets/Scripts/Interaction/HamsterWheel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using LotsOfTowers.Interaction.Triggers;
+
+namespace LotsOfTowers.Interaction
+{
+    public class HamsterWheel : MonoBehaviour
+    {
+        public GameObject rotateTrigger;
+        public bool isBreakable;
+
+        private WheelRotateTrigger rotateTriggerScript;
+        private float rotateSpeed = 0f;
+        private int speedGain = 1;
+        private int speedLoss = 3;
+
+        void Start()
+        {
+            rotateTriggerScript = rotateTrigger.GetComponent<WheelRotateTrigger>();
+        }
+
+        void Update()
+        {
+            if (rotateTriggerScript.GetPlayerRunning())
+                if (rotateSpeed < 15)
+                    rotateSpeed += speedGain;
+
+            if (!rotateTriggerScript.GetPlayerRunning())
+                if (rotateSpeed > 0)
+                    rotateSpeed -= speedLoss * Time.deltaTime;
+
+            if (rotateSpeed > 1)
+                gameObject.transform.Rotate(Vector3.forward * rotateSpeed);
+        }
+
+        public float GetRotateSpeed() { return rotateSpeed; }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/WheelRotateTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+    public class WheelRotateTrigger : MonoBehaviour
+    {
+        public bool isBreakable;
+
+        private bool playerRunning = false;
+
+        private void OnTriggerStay(Collider coll)
+        {
+            playerRunning = true;
+        }
+
+        private void OnTriggerExit()
+        {
+            playerRunning = false;
+        }
+
+        public bool GetPlayerRunning() { return playerRunning; }
+    }
+}
