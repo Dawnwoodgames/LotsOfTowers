eb46e0a1cad0d51060550a00856f5e65c13abd2a
Sephy <bob.vankamp@gmail.com>
2015-12-08 10:25:39 +0100

    Fixed blockbug

--- Assets/Scripts/Interaction/Triggers/FractureReset.cs
+++ Assets/Scripts/Interaction/Triggers/FractureReset.cs
@@ -7,12 +7,12 @@ namespace LotsOfTowers.Interaction.Triggers
 	public class FractureReset : MonoBehaviour
 	{
 		public Vector3 resetPosition = new Vector3();
-		public float resetIfBlow = 18;
+		public float resetIfBelow = -0.1f;
 		private Vector3 currentPosition;
 		// Update is called once per frame
 		void Update()
 		{
-			if (transform.position.y < resetIfBlow)
+			if (transform.localPosition.y < resetIfBelow)
 			{
 				if(!GetComponent<Rigidbody>().isKinematic)
 				{
