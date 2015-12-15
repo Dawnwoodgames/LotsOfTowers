296368219951771f41244be269dafd55d792f60a
Sephy <bob.vankamp@gmail.com>
2015-12-15 11:30:36 +0100

    Level slider update

--- Assets/Scripts/Framework/LevelSlider.cs
+++ Assets/Scripts/Framework/LevelSlider.cs
@@ -25,25 +25,18 @@ namespace LotsOfTowers.Framework
 			if (moveUp)
 				for (int obj = 0; obj < toMove.Length; obj++)
 				{
-                    if (toMove[obj].transform.position.y < oldPosition[obj].y + increaseBy)
-                        toMove[obj].transform.position = toMove[obj].transform.position + new Vector3(0, increaseBy * Time.deltaTime * speed, 0);
-                    else
-                    {
-                        toMove[obj].transform.position = new Vector3(toMove[obj].transform.position.x, oldPosition[obj].y + increaseBy, toMove[obj].transform.position.z);
+                    toMove[obj].transform.position = Vector3.MoveTowards(toMove[obj].transform.position, new Vector3(oldPosition[obj].x, oldPosition[obj].y+increaseBy, oldPosition[obj].z), Time.deltaTime * speed);
+                    if (toMove[obj].transform.position == new Vector3(oldPosition[obj].x, oldPosition[obj].y+increaseBy, oldPosition[obj].z))
                         isActive = false;
-                    }
-				}
+                }
 			else
 				for (int obj = 0; obj < toMove.Length; obj++)
 				{
-                    if (toMove[obj].transform.position.y > oldPosition[obj].y)
-                        toMove[obj].transform.position = toMove[obj].transform.position + new Vector3(0, -increaseBy * Time.deltaTime * speed, 0);
-                    else
-                    {
-                        toMove[obj].transform.position = oldPosition[obj];
+                    toMove[obj].transform.position = Vector3.MoveTowards(toMove[obj].transform.position, new Vector3(oldPosition[obj].x, oldPosition[obj].y, oldPosition[obj].z), Time.deltaTime * speed);
+                    if (toMove[obj].transform.position == new Vector3(oldPosition[obj].x, oldPosition[obj].y, oldPosition[obj].z))
                         isActive = false;
-                    }
 				}
+            Debug.Log(isActive);
 		}
 
 		void OnTriggerEnter(Collider other)
