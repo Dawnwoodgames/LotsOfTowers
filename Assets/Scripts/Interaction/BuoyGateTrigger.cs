1c7bac9136cde918b5dcbd76893d27a1a20d7357
Choraimy <Choraimy@145-24-244-205.wlan.hro.nl>
2015-12-16 11:58:33 +0100

    Added gates between buoys (BuoyGateTrigger) Added maze script (WaterMazeManager)

---
+++ Assets/Scripts/Interaction/BuoyGateTrigger.cs
@@ -0,0 +1,45 @@
+﻿using LotsOfTowers.Environment;
+using UnityEngine;
+
+namespace LotsOfTowers.Interaction {
+	[RequireComponent(typeof(BoxCollider))]
+	public sealed class BuoyGateTrigger : MonoBehaviour {
+		private WaterMazeManager manager;
+		private Buoy leftBuoy, rightBuoy;
+		private float timer;
+
+		public float delay = 0.5f; // Seconds between triggers
+
+		public void Awake() {
+			RaycastHit hit;
+
+			GetComponent<BoxCollider>().isTrigger = true;
+			this.manager = FindObjectOfType<WaterMazeManager>();
+
+			// Try and find a buoy on the left
+			Physics.Raycast(transform.position, transform.rotation * (Vector3.left * 5), out hit);
+			leftBuoy = hit.collider.GetComponent<Buoy>();
+
+			// Try and find a buoy on the right
+			Physics.Raycast(transform.position, transform.rotation * (Vector3.right * 5), out hit);
+			rightBuoy = hit.collider.GetComponent<Buoy>();
+		}
+
+		public void OnTriggerEnter(Collider coll) {
+			if (timer > 0) {
+				return;
+			}
+			manager.GateOpened (this, leftBuoy.Red || rightBuoy.Red);
+		}
+
+		public void OnTriggerExit(Collider coll) {
+			timer = delay;
+		}
+
+		public void Update() {
+			if (timer > 0) {
+				timer -= Time.deltaTime;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Interaction/WaterMazeManager.cs
@@ -0,0 +1,34 @@
+﻿using LotsOfTowers.Environment;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace LotsOfTowers.Interaction {
+	public sealed class WaterMazeManager : MonoBehaviour {
+		private List<BuoyGateTrigger> gates;
+		private bool respawnOnNext;
+
+		public void Awake() {
+			this.gates = new List<BuoyGateTrigger>();
+		}
+
+		public void GateOpened(BuoyGateTrigger gate, bool hasRedBuoy) {
+			if (hasRedBuoy || gates.Contains(gate)) {
+				respawnOnNext = true;
+			}
+
+			gates.Add(gate);
+		}
+
+		public void OnGUI() {
+			GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "Maze progress: " + gates.Count + " / 22");
+		}
+
+		public void Update() {
+			if (respawnOnNext || gates.Count > 22) {
+				GameManager.Instance.PlayerPassOutAndRespawn(transform);
+				gates.Clear();
+			}
+		}
+	}
+}
