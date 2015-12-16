46bd032a34673e5331ac8981ee9ceca1aac0e351
Sephy <bob.vankamp@gmail.com>
2015-12-16 11:18:54 +0100

    Batterij fixje

---
+++ Assets/Scripts/Environment/Battery.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace LotsOfTowers.Environment {
+
+	public sealed class Battery : MonoBehaviour {
+		private float internalClock;
+		private float originalY;
+
+
+		public void Awake() {
+			this.internalClock = Random.Range(0f, 1f);
+			this.originalY = transform.position.y;
+		}
+
+		public void FixedUpdate() {
+			internalClock += Time.deltaTime;
+			transform.position = new Vector3(transform.position.x, Mathf.Sin(internalClock) * 0.25f + originalY, transform.position.z);
+		}
+	}
+}
