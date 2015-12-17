7473d34f76330a0a3896b8e8beddac4fe9147d0f
Lars <larsh1991@hotmail.com>
2015-12-17 14:41:37 +0100

    Segemnet 1 done

--- Assets/FaucetWaves.cs
+++ Assets/FaucetWaves.cs
@@ -4,25 +4,44 @@ using System.Collections;
 public class FaucetWaves : MonoBehaviour {
 
     public GameObject waveObject;
-    public Transform endMarker;
+    public GameObject waterfall;
+    public ParticleSystem faucetWater;
     private float speed = 3.0f;
     private float startTime;
+    private bool faucetActive = true;
 
 	void Start () {
         waveObject = Instantiate(waveObject, new Vector3(-2.8f, 8.5f, -2.8f), Quaternion.identity) as GameObject;
 	}
 	
 	void Update () {
-        startTime += Time.deltaTime;
-        waveObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        waveObject.transform.Translate(-Vector3.left * speed * Time.deltaTime);
-
-        if (startTime >= 1.5f)
+        if (faucetActive)
         {
-            Destroy(waveObject);
-            waveObject = Instantiate(waveObject, new Vector3(-2.8f, 8.5f, -2.8f), Quaternion.identity) as GameObject;
-            waveObject.name = "Wave";
-            startTime = 0;
+            startTime += Time.deltaTime;
+            waveObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            waveObject.transform.Translate(-Vector3.left * speed * Time.deltaTime);
+
+            if (startTime >= 1.5f)
+            {
+                Destroy(waveObject);
+                waveObject = Instantiate(waveObject, new Vector3(-2.8f, 8.5f, -2.8f), Quaternion.identity) as GameObject;
+                waveObject.name = "Wave";
+                startTime = 0;
+            }
         }
+    }
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if (coll.tag == "Player" && Input.GetButtonDown("Submit"))
+            TurnOffWater();
+    }
+
+    private void TurnOffWater()
+    {
+        faucetActive = false;
+        faucetWater.Stop();
+        Destroy(waterfall);
+        Destroy(waveObject);
     }
 }
