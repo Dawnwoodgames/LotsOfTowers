3ad97c663d0e5739af65f050f7925794f0950cfd
Axel Schelfhout <axelschelfhout@gmail.com>
2015-12-17 14:47:18 +0100

    Start on segment2

---
+++ Assets/Scripts/Interaction/BathPlugPull.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction
+{
+
+    public class BathPlugPull : MonoBehaviour {
+
+        void Start()
+        {
+            
+        }
+
+	    void FixedUpdate()
+        {
+
+        }
+
+        
+    }
+
+}
---
+++ Assets/Scripts/Interaction/Triggers/Plug.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+
+    public class Plug : MonoBehaviour {
+
+        //
+
+	   
+    }
+
+}
