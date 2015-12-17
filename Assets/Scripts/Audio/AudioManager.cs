89bd10856cb8ffa7a738a3b3615d085ec5b88703
Axel Schelfhout <axelschelfhout@gmail.com>
2015-12-17 16:15:11 +0100

    Add soundEffects. Add this to the Player.cs by adding code which is put in development chat on slack

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Audio
+{
+    public class AudioManager : MonoBehaviour {
+
+        public GameObject soundEffects;
+        public GameObject backgroundSounds;
+
+        public AudioClip switchOnesieToDefaultSoundFile;
+        public AudioClip switchOnesieToElephantSoundFile;
+        public AudioClip switchOnesieToHamsterSoundFile;
+
+        public AudioClip[] enviromentSounds;
+        
+        public AudioClip onesieSwitchDefaultSound { get; set; }
+        public AudioClip onesieSwitchElephantSound { get; set; }
+        public AudioClip onesieSwitchHamsterSound { get; set; }
+
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+
+            onesieSwitchDefaultSound = switchOnesieToDefaultSoundFile;
+            onesieSwitchElephantSound = switchOnesieToElephantSoundFile;
+            onesieSwitchHamsterSound = switchOnesieToHamsterSoundFile;
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public void PlaySoundeffect(AudioClip sound)
+        {
+            soundEffects.GetComponent<AudioSource>().clip = sound;
+            soundEffects.GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
+
