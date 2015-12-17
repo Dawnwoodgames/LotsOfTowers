2f970591c626f0e0b4a5cd5b0246d41ef1403220
Lars <larsh1991@hotmail.com>
2015-12-17 13:48:58 +0100

    Waves implemented

---
+++ Assets/FaucetWaves.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaucetWaves : MonoBehaviour {
+
+    public GameObject waveObject;
+    public Transform endMarker;
+    private float speed = 3.0f;
+    private float startTime;
+
+	void Start () {
+        waveObject = Instantiate(waveObject, new Vector3(-2.8f, 8.5f, -2.8f), Quaternion.identity) as GameObject;
+	}
+	
+	void Update () {
+        startTime += Time.deltaTime;
+        waveObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        waveObject.transform.Translate(-Vector3.left * speed * Time.deltaTime);
+
+        if (startTime >= 1.5f)
+        {
+            Destroy(waveObject);
+            waveObject = Instantiate(waveObject, new Vector3(-2.8f, 8.5f, -2.8f), Quaternion.identity) as GameObject;
+            waveObject.name = "Wave";
+            startTime = 0;
+        }
+    }
+}
---
+++ Assets/WaveTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+    public class WaveTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                coll.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.Impulse);
+            }
+        }
+    }
+}
