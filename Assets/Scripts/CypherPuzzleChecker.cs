57376dd9aeb580c24f6c936201f696be10ed9469
Lars <larsh1991@hotmail.com>
2016-01-04 13:38:54 +0100

    Cypher puzzle complete

---
+++ Assets/Scripts/CypherPuzzleChecker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers
+{
+    public class CypherPuzzleChecker : MonoBehaviour
+    {
+        public GameObject oven, furnaceDoor;
+
+        private bool slotOneComplete, slotTwoComplete, slotThreeComplete = false;
+
+        void Update()
+        {
+            if (slotOneComplete && slotTwoComplete && slotThreeComplete)
+            {
+                oven.GetComponent<ParticleSystem>().Play();
+                furnaceDoor.GetComponent<Interaction.Triggers.FurnaceDoor>().cypherComplete = true;
+            }
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.name == "slot_1_complete")
+                slotOneComplete = true;
+
+            if (coll.name == "slot_2_complete")
+                slotTwoComplete = true;
+
+            if (coll.name == "slot_3_complete")
+                slotThreeComplete = true;
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.name == "slot_1_complete")
+                slotOneComplete = false;
+
+            if (coll.name == "slot_2_complete")
+                slotTwoComplete = false;
+
+            if (coll.name == "slot_3_complete")
+                slotThreeComplete = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/FurnaceDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LotsOfTowers.Interaction.Triggers
+{
+    public class FurnaceDoor : MonoBehaviour
+    {
+        public bool cypherComplete = false;
+        private bool playerInArea = false;
+        private int doorOpenAngle = -90;
+        private int doorCloseAngle = 0;
+        private float smooth = 2;
+
+        void Update()
+        {
+            if (cypherComplete)
+            {
+                Quaternion target = Quaternion.Euler(0, doorOpenAngle, 0);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
+            }
+        }
+    }
+}
