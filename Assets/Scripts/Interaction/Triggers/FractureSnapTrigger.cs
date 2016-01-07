0303621b6832f7f348f71401aff780fa1eb622f1
Lars <larsh1991@hotmail.com>
2016-01-07 15:46:38 +0100

    Fractures are now highlighted only when movable

--- Assets/Scripts/Interaction/Triggers/FractureSnapTrigger.cs
+++ Assets/Scripts/Interaction/Triggers/FractureSnapTrigger.cs
@@ -11,6 +11,8 @@ namespace LotsOfTowers.Interaction.Triggers
 				coll.transform.position = gameObject.transform.position;
 				coll.transform.rotation = new Quaternion(0, 0, 0, 0);
 				coll.GetComponent<Rigidbody>().isKinematic = true;
+                coll.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                coll.GetComponent<InteractableObjectOutline>().enabled = false;
 			}
 		}
 	}
