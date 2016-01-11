5fd741bb0a39af4d14138435b3b785d7f99cb29a
JayDijkstra <jaydijkstra@gmail.com>
2016-01-11 13:59:53 +0100

    Segment 1 of Tower 5 proto

---
+++ Assets/Scripts/Interaction/ScaryStatue.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+namespace Nimbi.Interaction
+{
+
+    public class ScaryStatue : MonoBehaviour
+    {
+
+        public bool isScary;
+
+        private Player player;
+        private GameObject scaryStatue;
+        private bool inTrigger;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            scaryStatue = GameObject.Find("Scary-Statue");
+            inTrigger = false;
+            isScary = true;
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (Input.GetButtonDown("Submit") && inTrigger && isScary)
+            {
+                scaryStatue.transform.Rotate(180, 0, 0);
+                isScary = false;
+            }
+        }
+
+        private void OnTriggerStay(Collider coll)
+        {
+            if (coll.attachedRigidbody)
+            {
+                inTrigger = true;
+            }
+        }
+
+
+
+    }
+}
+ 
---
+++ Assets/Scripts/Interaction/Triggers/DragonTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class DragonTrigger : MonoBehaviour {
+
+        private ScaryStatue scaryStatue;
+        private bool inTrigger;
+
+        // Use this for initialization
+        void Start() {
+            scaryStatue = GameObject.Find("Scary-Statue").GetComponent<ScaryStatue>();
+            inTrigger = false;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if   (Input.GetButtonDown("Submit") && inTrigger && scaryStatue.isScary)
+            {
+
+            }
+           
+            
+        }
+
+        private void OnTriggerStay(Collider coll)
+        {
+           if(coll.attachedRigidbody)
+            {
+                inTrigger = true;
+            }
+        }
+    }
+}
