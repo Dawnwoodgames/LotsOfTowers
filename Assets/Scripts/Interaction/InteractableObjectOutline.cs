9b739bba6975e4380375417a489868473b6c0357
Axel Schelfhout <axelschelfhout@gmail.com>
2016-01-12 14:10:26 +0100

    Remove debug msg

--- Assets/Scripts/Interaction/InteractableObjectOutline.cs
+++ Assets/Scripts/Interaction/InteractableObjectOutline.cs
@@ -32,7 +32,7 @@ namespace Nimbi.Interaction
         void FixedUpdate()
         {
             distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
-            Debug.Log(distance);
+            
             if (distance < 3)
             {
                 HighlightArea();
