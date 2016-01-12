318aff8b242bafda737c764d0e7bd17c8827c6dd
Choraimy <choraimy@gmail.com>
2016-01-12 10:48:38 +0100

    Removed editor checks from pause menu and moved them into the game manager Refactored game manager

--- Assets/Scripts/Framework/GameManager.cs
+++ Assets/Scripts/Framework/GameManager.cs
@@ -9,320 +9,313 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Nimbi
-{
-	[RequireComponent(typeof(Canvas))]
-	[RequireComponent(typeof(CanvasRenderer))]
-	public class GameManager : MonoBehaviour
-	{
-		public const float FadeDuration = 0.5f;
+namespace Nimbi {
+    [RequireComponent(typeof(Canvas))]
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class GameManager : MonoBehaviour {
+        public const float FadeDuration = 0.5f;
 
-		private static GameManager instance;
-		private Canvas canvas;
-		private Image fader;
-		private bool hasStarted;
-		private Image loadingScreen;
-		private Sprite loadingSpriteA, loadingSpriteB;
-		private Player player;
-		private PlayerController playerController;
-		private Transform spawnPoint;
+        private static GameManager instance;
+        private Canvas canvas;
+        private Image fader;
+        private bool hasStarted;
+        private Image loadingScreen;
+        private Sprite loadingSpriteA, loadingSpriteB;
+        private Player player;
+        private PlayerController playerController;
+        private Transform spawnPoint;
 
-		public static GameManager Instance {
-			get {
-				if (instance == null) {
-					instance = new GameObject("Game Manager", typeof(GameManager)).GetComponent<GameManager>();
-				}
-				return instance;
-			}
-		}
+        public static GameManager Instance {
+            get {
+                if (instance == null) {
+                    instance = new GameObject("Game Manager", typeof(GameManager)).GetComponent<GameManager>();
+                }
+                return instance;
+            }
+        }
 
-		public bool CursorEnabled {
-			get { return Cursor.lockState == CursorLockMode.None && Cursor.visible; }
-			set {
-				Cursor.lockState = value ? CursorLockMode.None : CursorLockMode.Locked;
-				Cursor.visible = value;
-			}
-		}
+        public bool CursorEnabled {
+            get { return Cursor.lockState == CursorLockMode.None && Cursor.visible; }
+            set {
+                if (!Application.isEditor) {
+                    Cursor.lockState = value ? CursorLockMode.None : CursorLockMode.Locked;
+                    Cursor.visible = value;
+                }
+            }
+        }
 
-        public bool JoystickConnected
-        {
+        public bool JoystickConnected {
             get { return Input.GetJoystickNames().Length > 0 && Input.GetJoystickNames().Where(s => s != String.Empty).Count() > 0; }
         }
 
-		public string Language
-		{ // Default: en
-			get { return PlayerPrefs.HasKey("Language") ? PlayerPrefs.GetString("Language") : "en"; }
-			set
-			{
-				if (LanguageManager.Instance.GetSupportedLanguages().Where(l => l.languageCode == value).Count() != 0)
-				{
-					LanguageManager.Instance.ChangeLanguage(value);
-					PlayerPrefs.SetString("Language", value);
-				}
-			}
-		}
-		
-		public Transform SpawnPoint {
-            get; set;
-		}
-		
-		public void Awake()
-		{
-			if (FindObjectsOfType<GameManager>().Length > 1) {
-				Destroy(gameObject);
-			} else {
-				GameManager.instance = this;
-			}
-
-			DontDestroyOnLoad(this);
-			LanguageManager.Instance.ChangeLanguage(Language);
-			LanguageManager.Instance.name = "Language Manager";
-			LanguageManager.Instance.transform.SetParent(transform, false);
-			OnLevelWasLoaded(SceneManager.GetActiveScene().buildIndex);
-
-			this.canvas = GetComponent<Canvas>();
-			this.fader = new GameObject("Transition Fader", typeof(Image)).GetComponent<Image>();
-			this.loadingScreen = new GameObject("Loading Screen", typeof(Image)).GetComponent<Image>();
-			this.loadingSpriteA = Resources.Load<Sprite>("UI/LoadingScreenLoading");
-			this.loadingSpriteB = Resources.Load<Sprite>("UI/LoadingScreenDone");
-            Instantiate(Resources.Load("Prefabs/AudioManager"));
+        public string Language { // Default: en
+            get { return PlayerPrefs.HasKey("Language") ? PlayerPrefs.GetString("Language") : "en"; }
+            set {
+                if (LanguageManager.Instance.GetSupportedLanguages().Where(l => l.languageCode == value).Count() != 0) {
+                    LanguageManager.Instance.ChangeLanguage(value);
+                    PlayerPrefs.SetString("Language", value);
+                }
+            }
         }
 
-		public void FadeIn() {
-			StopAllCoroutines();
-			StartCoroutine(FadeInCoroutine());
-		}
+        public Transform SpawnPoint {
+            get; set;
+        }
 
-		private IEnumerator FadeInCoroutine() {
-			while (!hasStarted) {
-				yield return null;
-			}
+        public void Awake() {
+            if (FindObjectsOfType<GameManager>().Length > 1) {
+                Destroy(gameObject);
+            } else {
+                GameManager.instance = this;
+            }
 
-			while (fader.color.a > 0.01f) {
-				fader.color = new Color(
-					fader.color.r,
-					fader.color.g,
-					fader.color.b,
-					fader.color.a - Time.deltaTime / FadeDuration
-				);
-				yield return null;
-			}
-		}
+            DontDestroyOnLoad(this);
+            Instantiate(Resources.Load("Prefabs/AudioManager"));
+            LanguageManager.Instance.ChangeLanguage(Language);
+            LanguageManager.Instance.name = "Language Manager";
+            LanguageManager.Instance.transform.SetParent(transform, false);
+            OnLevelWasLoaded(SceneManager.GetActiveScene().buildIndex);
 
-		public void FadeOut() {
-			StopAllCoroutines();
-			StartCoroutine(FadeOutCoroutine());
-		}
+            this.canvas = GetComponent<Canvas>();
+            this.fader = new GameObject("Transition Fader", typeof(Image)).GetComponent<Image>();
+            this.loadingScreen = new GameObject("Loading Screen", typeof(Image)).GetComponent<Image>();
+            this.loadingSpriteA = Resources.Load<Sprite>("UI/LoadingScreenLoading");
+            this.loadingSpriteB = Resources.Load<Sprite>("UI/LoadingScreenDone");
+        }
 
-		private IEnumerator FadeOutCoroutine() {
-			while (!hasStarted) {
-				yield return null;
-			}
+        public void FadeIn() {
+            StopAllCoroutines();
+            StartCoroutine(FadeInCoroutine());
+        }
 
-			while (fader.color.a < 0.99f) {
-				fader.color = new Color(
-					fader.color.r,
-					fader.color.g,
-					fader.color.b,
-					fader.color.a + Time.deltaTime / FadeDuration
-				);
-				yield return null;
-			}
-		}
+        private IEnumerator FadeInCoroutine() {
+            while (!hasStarted) {
+                yield return null;
+            }
 
-		public void HideFader() {
-			fader.color = Color.clear;
-		}
+            while (fader.color.a > 0.01f) {
+                fader.color = new Color(
+                    fader.color.r,
+                    fader.color.g,
+                    fader.color.b,
+                    fader.color.a - Time.deltaTime / FadeDuration
+                );
+                yield return null;
+            }
+        }
 
-		public void LoadLevel(int index) {
-			LoadLevel(index, false);
-		}
+        public void FadeOut() {
+            StopAllCoroutines();
+            StartCoroutine(FadeOutCoroutine());
+        }
 
-		public void LoadLevel(int index, bool forceUnlock) {
-			if (index == -1) {
-				index = SceneManager.GetActiveScene().buildIndex;
-			} else if (!forceUnlock && PlayerPrefs.GetInt("bIsLevelAvailable" + index, 0) == 0) {
-				return;
-			}
+        private IEnumerator FadeOutCoroutine() {
+            while (!hasStarted) {
+                yield return null;
+            }
 
-			PlayerPrefs.SetInt("bIsLevelAvailable" + index, 1);
-			Time.timeScale = 1;
-			StopAllCoroutines();
-			StartCoroutine(LoadLevelCoroutine(index));
-		}
+            while (fader.color.a < 0.99f) {
+                fader.color = new Color(
+                    fader.color.r,
+                    fader.color.g,
+                    fader.color.b,
+                    fader.color.a + Time.deltaTime / FadeDuration
+                );
+                yield return null;
+            }
+        }
 
-		private IEnumerator LoadLevelCoroutine(int index) {
-			if (playerController != null) {
-				playerController.enabled = false;
-			}
+        public void HideFader() {
+            fader.color = Color.clear;
+        }
 
-			while (!hasStarted) {
-				yield return null;
-			}
-			
-			while (fader.color.a < 0.99f) {
-				fader.color = new Color(
-					fader.color.r,
-					fader.color.g,
-					fader.color.b,
-					fader.color.a + Time.deltaTime / FadeDuration
-				);
-				yield return null;
-			}
+        public void LoadLevel(int index) {
+            LoadLevel(index, false);
+        }
 
-			if (index != 0) {
-				// If the scene to be loaded is NOT the main menu, show the loading screen
-				loadingScreen.sprite = loadingSpriteA;
-				loadingScreen.enabled = true;
-			}
+        public void LoadLevel(int index, bool forceUnlock) {
+            if (index == -1) {
+                index = SceneManager.GetActiveScene().buildIndex;
+            } else if (!forceUnlock && PlayerPrefs.GetInt("bIsLevelAvailable" + index, 0) == 0) {
+                return;
+            }
+
+            PlayerPrefs.SetInt("bIsLevelAvailable" + index, 1);
+            Time.timeScale = 1;
+            StopAllCoroutines();
+            StartCoroutine(LoadLevelCoroutine(index));
+        }
+
+        private IEnumerator LoadLevelCoroutine(int index) {
+            if (playerController != null) {
+                playerController.enabled = false;
+            }
+
+            while (!hasStarted) {
+                yield return null;
+            }
+
+            while (fader.color.a < 0.99f) {
+                fader.color = new Color(
+                    fader.color.r,
+                    fader.color.g,
+                    fader.color.b,
+                    fader.color.a + Time.deltaTime / FadeDuration
+                );
+                yield return null;
+            }
+
+            if (index != 0) {
+                // If the scene to be loaded is NOT the main menu, show the loading screen
+                loadingScreen.sprite = loadingSpriteA;
+                loadingScreen.enabled = true;
+            }
 
             SceneManager.LoadSceneAsync(index);
-			yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(1);
 
-			if (index != 0) {
-				loadingScreen.sprite = loadingSpriteB;
+            if (index != 0) {
+                loadingScreen.sprite = loadingSpriteB;
 
-				while (Input.GetAxis("Submit") == 0) {
-					yield return null;
-				}
+                while (Input.GetAxis("Submit") == 0) {
+                    yield return null;
+                }
 
-				loadingScreen.enabled = false;
-			}
-			
-			while (fader.color.a > 0.01f) {
-				fader.color = new Color(
-					fader.color.r,
-					fader.color.g,
-					fader.color.b,
-					fader.color.a - Time.deltaTime / FadeDuration
-				);
-				yield return null;
-			}
+                loadingScreen.enabled = false;
+            }
 
-			if (playerController != null) {
-				playerController.enabled = true;
-			}
-		}
-		
-		public void OnLevelWasLoaded(int index) {
-			try {
-				player = FindObjectOfType<Player>();
-				playerController = FindObjectOfType<PlayerController>();
+            while (fader.color.a > 0.01f) {
+                fader.color = new Color(
+                    fader.color.r,
+                    fader.color.g,
+                    fader.color.b,
+                    fader.color.a - Time.deltaTime / FadeDuration
+                );
+                yield return null;
+            }
+
+            if (playerController != null) {
+                playerController.enabled = true;
+            }
+        }
+
+        public void OnLevelWasLoaded(int index) {
+            try {
+                player = FindObjectOfType<Player>();
+                playerController = FindObjectOfType<PlayerController>();
                 SpawnPoint = GameObject.Find("Level/Spawn Point").transform;
             } catch (Exception) { }
-		}
+        }
 
-		public void PlayerPassOutAndRespawn(Transform spawnPoint) {
-			StopAllCoroutines();
-			StartCoroutine(PlayerPassOutAndRespawnCoroutine(spawnPoint));
-		}
+        public void PlayerPassOutAndRespawn(Transform spawnPoint) {
+            StopAllCoroutines();
+            StartCoroutine(PlayerPassOutAndRespawnCoroutine(spawnPoint));
+        }
 
-		private IEnumerator PlayerPassOutAndRespawnCoroutine(Transform spawnPoint) {
+        private IEnumerator PlayerPassOutAndRespawnCoroutine(Transform spawnPoint) {
             HamsterBall ball = FindObjectOfType<HamsterBall>();
 
-			if (player != null && playerController != null) {
+            if (player != null && playerController != null) {
                 if (ball != null && ball.playerInside) {
                     ball.enabled = false;
                 }
-				playerController.enabled = false;
+                playerController.enabled = false;
 
-				while (!hasStarted) {
-					yield return null;
-				}
+                while (!hasStarted) {
+                    yield return null;
+                }
 
-				while (fader.color.a < 0.99f) {
-					fader.color = new Color(
-						fader.color.r,
-						fader.color.g,
-						fader.color.b,
-						fader.color.a + Time.deltaTime / FadeDuration
-					);
-					yield return null;
-				}
+                while (fader.color.a < 0.99f) {
+                    fader.color = new Color(
+                        fader.color.r,
+                        fader.color.g,
+                        fader.color.b,
+                        fader.color.a + Time.deltaTime / FadeDuration
+                    );
+                    yield return null;
+                }
 
-                if (ball != null && ball.playerInside)
-                {
+                if (ball != null && ball.playerInside) {
                     ball.Ball.transform.position = spawnPoint.position;
                     ball.Ball.transform.rotation = spawnPoint.rotation;
                 }
                 player.transform.position = spawnPoint.position;
-				player.transform.rotation = spawnPoint.rotation;
+                player.transform.rotation = spawnPoint.rotation;
 
-				while (fader.color.a > 0.01f) {
-					fader.color = new Color(
-						fader.color.r,
-						fader.color.g,
-						fader.color.b,
-						fader.color.a - Time.deltaTime / FadeDuration
-					);
-					yield return null;
-				}
+                while (fader.color.a > 0.01f) {
+                    fader.color = new Color(
+                        fader.color.r,
+                        fader.color.g,
+                        fader.color.b,
+                        fader.color.a - Time.deltaTime / FadeDuration
+                    );
+                    yield return null;
+                }
 
-				playerController.enabled = true;
-                if (ball != null && ball.playerInside)
-                {
+                playerController.enabled = true;
+                if (ball != null && ball.playerInside) {
                     ball.enabled = true;
                 }
             }
-		}
+        }
 
-		public void Quit() {
-			StopAllCoroutines();
-			StartCoroutine(QuitCoroutine());
-		}
+        public void Quit() {
+            StopAllCoroutines();
+            StartCoroutine(QuitCoroutine());
+        }
 
-		private IEnumerator QuitCoroutine() {
-			while (!hasStarted) {
-				yield return null;
-			}
-				
-			while (fader.color.a < 0.99f) {
-				fader.color = new Color(
-					fader.color.r,
-					fader.color.g,
-					fader.color.b,
-					fader.color.a + Time.deltaTime / FadeDuration
-				);
-				yield return null;
-			}
+        private IEnumerator QuitCoroutine() {
+            while (!hasStarted) {
+                yield return null;
+            }
 
-			Application.Quit ();
-		}
+            while (fader.color.a < 0.99f) {
+                fader.color = new Color(
+                    fader.color.r,
+                    fader.color.g,
+                    fader.color.b,
+                    fader.color.a + Time.deltaTime / FadeDuration
+                );
+                yield return null;
+            }
 
-		public void ShowTooltip(string resourceName, string axis) {
-			ShowTooltip(resourceName, axis, false);
-		}
+            Application.Quit();
+        }
 
-		public void ShowTooltip(string resourceName, string axis, bool useCookie) {			
-			if (!useCookie || PlayerPrefs.GetInt("UITooltip" + resourceName, 0) == 0) {
-				PlayerPrefs.SetInt("UITooltip" + resourceName, 1);
-				UITooltip tooltip = new GameObject("UITooltip", typeof(UITooltip)).GetComponent<UITooltip>();
+        public void ShowTooltip(string resourceName, string axis) {
+            ShowTooltip(resourceName, axis, false);
+        }
 
-				tooltip.gameObject.transform.SetParent(transform, false);
-				tooltip.SetAxis(axis);
-				tooltip.SetSprite(Resources.Load<Sprite>("UI/Tooltip" + resourceName));
-			}
-		}
+        public void ShowTooltip(string resourceName, string axis, bool useCookie) {
+            if (!useCookie || PlayerPrefs.GetInt("UITooltip" + resourceName, 0) == 0) {
+                PlayerPrefs.SetInt("UITooltip" + resourceName, 1);
+                UITooltip tooltip = new GameObject("UITooltip", typeof(UITooltip)).GetComponent<UITooltip>();
 
-		public void Start() {
-			canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-			canvas.sortingOrder = Int16.MaxValue;
+                tooltip.gameObject.transform.SetParent(transform, false);
+                tooltip.SetAxis(axis);
+                tooltip.SetSprite(Resources.Load<Sprite>("UI/Tooltip" + resourceName));
+            }
+        }
 
-			// Transition Fader setup
-			fader.color = Color.clear;
-			fader.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
-			fader.transform.SetParent(transform, false);
+        public void Start() {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = Int16.MaxValue;
 
-			// Loading Screen setup
-			var scale = Mathf.Round(Screen.width / 192) < Mathf.Round(Screen.height / 108) ?
-				Mathf.Round(Screen.width / 192) : Mathf.Round(Screen.height / 108);
-			loadingScreen.enabled = false;
-			loadingScreen.rectTransform.localScale = new Vector3(scale, scale, 1);
-			loadingScreen.rectTransform.sizeDelta = new Vector2(192, 108);
-			loadingScreen.sprite = loadingSpriteA;
-			loadingScreen.transform.SetParent(transform, false);
+            // Transition Fader setup
+            fader.color = Color.clear;
+            fader.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+            fader.transform.SetParent(transform, false);
 
-			hasStarted = true;
-		}
-	}
+            // Loading Screen setup
+            var scale = Mathf.Round(Screen.width / 192) < Mathf.Round(Screen.height / 108) ?
+                Mathf.Round(Screen.width / 192) : Mathf.Round(Screen.height / 108);
+            loadingScreen.enabled = false;
+            loadingScreen.rectTransform.localScale = new Vector3(scale, scale, 1);
+            loadingScreen.rectTransform.sizeDelta = new Vector2(192, 108);
+            loadingScreen.sprite = loadingSpriteA;
+            loadingScreen.transform.SetParent(transform, false);
+
+            hasStarted = true;
+        }
+    }
 }
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,50 +3,47 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Nimbi.UI {
-	[RequireComponent(typeof(Canvas))]
-	public class PauseMenu : MonoBehaviour {
-		private Canvas canvas;
-		private EventSystem eventSystem;
-		private bool wasCancelPressed;
+    [RequireComponent(typeof(Canvas))]
+    public class PauseMenu : MonoBehaviour {
+        private Canvas canvas;
+        private EventSystem eventSystem;
+        private bool wasCancelPressed;
 
-		public void Awake() {
-            if(!Application.isEditor)
-			    GameManager.Instance.CursorEnabled = false;
-			this.canvas = GetComponent<Canvas>();
-			this.eventSystem = FindObjectOfType<EventSystem>();
-			this.wasCancelPressed = false;
-		}
-		
-		public void Disable() {
-			canvas.enabled = false;
-			eventSystem.SetSelectedGameObject(null);
-			Time.timeScale = 1;
-		}
+        public void Awake() {
+            GameManager.Instance.CursorEnabled = false;
+            this.canvas = GetComponent<Canvas>();
+            this.eventSystem = FindObjectOfType<EventSystem>();
+            this.wasCancelPressed = false;
+        }
 
-		public void Enable() {
-			canvas.enabled = true;
+        public void Disable() {
+            canvas.enabled = false;
+            eventSystem.SetSelectedGameObject(null);
+            Time.timeScale = 1;
+        }
+
+        public void Enable() {
+            canvas.enabled = true;
             eventSystem.SetSelectedGameObject(GetComponentInChildren<Button>().gameObject);
-			GameManager.Instance.HideFader();
-			Time.timeScale = 0;
-		}
+            GameManager.Instance.HideFader();
+            Time.timeScale = 0;
+        }
 
-		public void Update() {
-			if (wasCancelPressed && !Input.GetButton("Cancel")) {
-				// Key up
-				if (canvas.enabled) {
-					Disable();
-                    if (!Application.isEditor)
-                        GameManager.Instance.CursorEnabled = false;
-				} else {
-					Enable();
-                    if (!Application.isEditor)
-                        GameManager.Instance.CursorEnabled = true;
-				}
-				wasCancelPressed = false;
-			} else if (Input.GetButton("Cancel")) {
-				// Key down
-				wasCancelPressed = true;
-			}
-		}
-	}
+        public void Update() {
+            if (wasCancelPressed && !Input.GetButton("Cancel")) {
+                // Key up
+                if (canvas.enabled) {
+                    Disable();
+                    GameManager.Instance.CursorEnabled = false;
+                } else {
+                    Enable();
+                    GameManager.Instance.CursorEnabled = true;
+                }
+                wasCancelPressed = false;
+            } else if (Input.GetButton("Cancel")) {
+                // Key down
+                wasCancelPressed = true;
+            }
+        }
+    }
 }
