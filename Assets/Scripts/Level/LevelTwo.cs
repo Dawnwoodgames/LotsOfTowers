6e0c9f443a994ac594fd48d7a8e558cd01fe4b83
Choraimy <choraimy@gmail.com>
2016-01-12 14:29:25 +0100

    Player can now have onesies on awake Tower 2 -> removed onesie adding script Tower 3 -> removed onesie adding script Added Dragon onesie asset

--- Assets/Scripts/Level/LevelTwo.cs
+++ Assets/Scripts/Level/LevelTwo.cs
@@ -9,9 +9,5 @@ namespace Nimbi.Level
         public void ModifySpawnPoint() {
             GameObject.Find("Spawn Point").transform.position = new Vector3(5.82f, 11.63f, 0);
 		}
-
-		public void Start() {
-			FindObjectOfType<Player>().AddOnesie(Resources.Load("OnesieElephant") as Onesie);
-		}
     }
 }
