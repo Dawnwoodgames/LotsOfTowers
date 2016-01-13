0e0ebfe169fd2ee11ed828cae048e5ed1c89bdfc
Tim Ververs <timververs@msn.com>
2016-01-13 13:33:09 +0100

    Enviornments toevoegen

---
+++ Assets/Scripts/Environment/Fans.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Nimbi.Environment
+{
+	public class Fans : MonoBehaviour
+	{
+		public Vector3 rotation;
+		
+		// Update is called once per frame
+		void Update()
+		{
+			transform.Rotate(rotation);
+		}
+	}
+}
