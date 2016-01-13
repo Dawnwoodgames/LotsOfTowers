797ce1a97a2959b75302de659d596a7de19a9b7a
Sephy <bob.vankamp@gmail.com>
2016-01-13 16:16:54 +0100

    Rope pickup

---
+++ Assets/Scripts/Interaction/RopePickup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction
+{
+    public class RopePickup : MonoBehaviour
+    {
+
+        private bool inTrigger;
+        private bool pickedUp;
+        public GameObject player;
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if(coll.tag == "Player")
+            {
+                inTrigger = true;
+            }
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                inTrigger = false;
+            }
+        }
+
+        void Update()
+        {
+            if(inTrigger && Input.GetButton("Submit"))
+            {
+                transform.parent = player.transform;
+            }
+        }
+    }
+}
