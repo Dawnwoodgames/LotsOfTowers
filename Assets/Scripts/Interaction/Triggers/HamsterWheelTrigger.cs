ca7d026e346ef0e78db0c1bf2e0af39b6b579c1c
Sephy <bob.vankamp@gmail.com>
2016-01-14 15:38:09 +0100

    Draaiscriptje update

--- Assets/Scripts/Interaction/Triggers/HamsterWheelTrigger.cs
+++ Assets/Scripts/Interaction/Triggers/HamsterWheelTrigger.cs
@@ -10,19 +10,23 @@ namespace Nimbi.Interaction.Triggers
         public bool x, y, z, negative;
         public float speed;
         private Vector3 rotation;
+        public Vector3 totalRotation;
+        private bool stopped;
 
         private bool playerRunning = false;
 
         void Start()
         {
             rotation = new Vector3();
+            totalRotation = new Vector3();
         }
 
         void Update()
         {
-            if (playerRunning)
+            if (playerRunning && !stopped)
             {
                 rotation = new Vector3(speed * (x ? 1 : 0), speed * (y ? 1 : 0), speed * (z ? 1 : 0))*(negative?-1:1);
+                totalRotation += rotation;
                 wheel.transform.Rotate(rotation.x, rotation.y, rotation.z);
             }
         }
@@ -40,5 +44,10 @@ namespace Nimbi.Interaction.Triggers
         }
 
         public bool GetPlayerRunning() { return playerRunning; }
+
+        public void Stop()
+        {
+            this.stopped = true;
+        }
     }
 }
