0acfdcb614bd11a9c10888598d2e187581de7e03
Lars <larsh1991@hotmail.com>
2016-01-14 14:47:50 +0100

    Rework HamsterWheel

---
+++ Assets/Scripts/Interaction/HamsterWheelRotateObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Interaction.Triggers;
+
+namespace Nimbi.Interaction
+{
+    [RequireComponent(typeof(HamsterWheelTrigger))]
+    public class HamsterWheelRotateObject : MonoBehaviour
+    {
+
+        public GameObject item;
+        private HamsterWheelTrigger wheel;
+
+        void Start()
+        {
+            wheel = GetComponent<HamsterWheelTrigger>();
+        }
+
+        void Update()
+        {
+            if (wheel.GetPlayerRunning())
+                item.transform.Rotate(0, 1f, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/HamsterWheelTrigger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class HamsterWheelTrigger : MonoBehaviour
+    {
+
+        public GameObject wheel;
+
+        private bool playerRunning = false;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (playerRunning)
+                wheel.transform.Rotate(3f, 0, 0);
+        }
+
+        private void OnTriggerStay(Collider coll)
+        {
+            if (coll.tag == "Player")
+                playerRunning = true;
+        }
+
+        private void OnTriggerExit(Collider coll)
+        {
+            if (coll.tag == "Player")
+                playerRunning = false;
+        }
+
+        public bool GetPlayerRunning() { return playerRunning; }
+    }
+}
