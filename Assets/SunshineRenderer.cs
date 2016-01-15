139bce479a77742f20f445f19abafe0d96d13ea0
Lars <larsh1991@hotmail.com>
2016-01-15 14:26:33 +0100

    Sunshine commit

--- Assets/SunshineRenderer.cs
+++ Assets/SunshineRenderer.cs
@@ -5,76 +5,67 @@ using System.Collections;
 [RequireComponent(typeof(LineRenderer))]
 public class SunshineRenderer : MonoBehaviour
 {
-    private LineRenderer lineRenderer;
-
+    private LineRenderer line;
     private Ray ray;
     private RaycastHit hit;
-
-    private Vector3 inDirection;
-
-    private int nReflections = 4;
-    private int nPoints;
+    private Vector3 inDirection, origin;
 
     private string input;
+    private int nReflections = 8;
+    private int nPoints;
 
     void Awake()
     {
-        lineRenderer = this.GetComponent<LineRenderer>();
+        line = this.GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        nReflections = Mathf.Clamp(nReflections, 1, nReflections);
-        ray = new Ray(transform.position, transform.forward);
+        nReflections = Mathf.Clamp(nReflections, 0, nReflections);
 
-        Debug.DrawRay(transform.position, transform.forward * 100, Color.magenta);
+        origin = transform.position;
+        ray = new Ray(origin, transform.forward);
+
+        Debug.DrawRay(origin, transform.forward * 100, Color.magenta);
 
         nPoints = nReflections;
-        lineRenderer.SetVertexCount(nPoints);
-        lineRenderer.SetPosition(0, transform.position);
+        line.SetVertexCount(nPoints);
+        line.SetPosition(0, origin);
 
-        for (int i = 0; i <= nReflections; i++)
+        for (int i = 0; i < nReflections; i++)
         {
             if (i == 0)
             {
                 if (Physics.Raycast(ray.origin, ray.direction, out hit, 100))
                 {
-                    inDirection = Vector3.Reflect(ray.direction, hit.normal);
-                    ray = new Ray(hit.point, inDirection);
-
-                    input = hit.transform.name;
-                    Match match = Regex.Match(input, @"Draai_Spiegel_\d+", RegexOptions.IgnoreCase);
-                    if (match.Success)
+                    if (hit.transform.tag == "Mirror")
                     {
-                        Debug.DrawRay(hit.point, hit.normal * 3, Color.blue);
-                        Debug.DrawRay(hit.point, inDirection * 100, Color.magenta);
+                        inDirection = Vector3.Reflect(ray.direction, hit.normal);
+                        ray = new Ray(hit.point, inDirection);
+
+                        Debug.DrawRay(hit.point, hit.normal * 30, Color.blue);
 
                         if (nReflections == 1)
-                            lineRenderer.SetVertexCount(++nPoints);
-
-                        lineRenderer.SetPosition(i + 1, hit.point);
+                        {
+                            line.SetVertexCount(++nPoints);
+                        }
                     }
-                    else { Debug.Log(input); }
+                    line.SetPosition(i + 1, hit.point);
                 }
-            }
-            else
+            } else
             {
-                if (Physics.Raycast(ray.origin, ray.direction, out hit, 100))
+                if (Physics.Raycast(ray.origin,ray.direction,out hit, 100))
                 {
-                    inDirection = Vector3.Reflect(inDirection, hit.normal);
-                    ray = new Ray(hit.point, inDirection);
-
-                    input = hit.transform.name;
-                    Match match = Regex.Match(input, @"Draai_Spiegel_\d+", RegexOptions.IgnoreCase);
-                    if (match.Success)
+                    if (hit.transform.tag == "Mirror")
                     {
-                        Debug.DrawRay(hit.point, hit.normal * 3, Color.blue);
-                        Debug.DrawRay(hit.point, inDirection * 100, Color.magenta);
+                        inDirection = Vector3.Reflect(inDirection, hit.normal);
+                        ray = new Ray(hit.point, inDirection);
 
-                        lineRenderer.SetVertexCount(++nPoints);
-                        lineRenderer.SetPosition(i + 1, hit.point);
+                        Debug.DrawRay(hit.point, hit.normal * 30, Color.blue);
+
+                        line.SetVertexCount(++nPoints);
                     }
-                    else { Debug.Log(input); }
+                    line.SetPosition(i + 1, hit.point);
                 }
             }
         }
