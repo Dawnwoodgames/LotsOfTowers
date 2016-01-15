f311ab240df8b8f8e71c021b44daa3d41d713345
Axel Schelfhout <axelschelfhout@gmail.com>
2016-01-15 12:12:50 +0100

    Wheel breaks when u run trough it, also installed this feature into level4

--- Assets/Scripts/Interaction/BrokenHamsterwheel.cs
+++ Assets/Scripts/Interaction/BrokenHamsterwheel.cs
@@ -7,38 +7,35 @@ namespace Nimbi.Interaction
 {
     public class BrokenHamsterwheel : MonoBehaviour
     {
-        public GameObject rotateTrigger;
+        public HamsterWheelTrigger rotateTrigger;
+        public WaterHole waterHole;
         public float maxDamage = 6.5f;
 
-        private WheelRotateTrigger rotateTriggerScript;
-        //private HamsterWheel wheel;
         private Player player;
         private float damage = 0;
-        
         private bool broken = false;
-        
+
         void Start()
         {
-            //wheel = GetComponent<HamsterWheel>();
-            rotateTriggerScript = rotateTrigger.GetComponent<WheelRotateTrigger>();
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         }
 
         void FixedUpdate()
         {
-            if (rotateTriggerScript.GetPlayerRunning() && !broken)
+            if (rotateTrigger.GetPlayerRunning() && !broken)
             {
+                Debug.Log(damage);
                 damage += 1 * Time.deltaTime;
-                if (damage > (maxDamage / 2))
+                if (damage > (maxDamage / 4))
                 {
-                    transform.Rotate(new Vector3(.3f, 0));
-                    if (damage > (maxDamage / 4))
+                    transform.Rotate(new Vector3(0, 0, .05f));
+                    if (damage > (maxDamage / 2))
                     {
-                        transform.Rotate(new Vector3(.6f, 0));
+                        transform.Rotate(new Vector3(0, 0, .15f));
                         if (damage > maxDamage)
                         {
-                            GameObject.Find("Water").GetComponent<WaterHole>().waterRising = false;
-                            transform.Rotate(new Vector3(1f, 0));
+                            waterHole.waterRising = false;
+                            transform.Rotate(new Vector3(0, 0, .3f));
                             if (player.Onesie.isHeavy)
                                 broken = true;
                         }
