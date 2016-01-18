321144663389926ede4ad9a1be88e8faa51d5385
Axel Schelfhout <axelschelfhout@gmail.com>
2016-01-18 11:06:52 +0100

    Add light on off script and lights

---
+++ Assets/Scripts/Interaction/LightOnOff.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction
+{
+    public class LightOnOff : MonoBehaviour {
+
+        public GameObject[] Lights;
+
+        void OnTriggerEnter(Collider col)
+        {
+            foreach(GameObject light in Lights)
+            {
+                light.SetActive(true);
+            }
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            foreach (GameObject light in Lights)
+            {
+                light.SetActive(false);
+            }
+        }
+    }
+}
