596a3b6be9bf974b39440ca80c08c776261e6996
Tim Ververs <timververs@msn.com>
2016-01-19 16:51:51 +0100

    Shadow's and lights + scripts to trigger fire + onesie dragon

---
+++ Assets/DragonShadowTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+
+public class DragonShadowTrigger : MonoBehaviour
+{
+	public Light dragonLight;
+	public Transform scaryGuy;
+	public bool isCompleted = false;
+	private bool showDragonShadow = false;
+
+	// Update is called once per frame
+	void OnTriggerStay(Collider coll)
+	{
+		if (coll.tag == "Player")
+		{
+			if (coll.GetComponent<Player>().Onesie.name == "Dragon" && dragonLight.isActiveAndEnabled)
+			{
+				showDragonShadow = true;
+			}
+		}
+	}
+
+	void Update()
+	{
+		if(showDragonShadow)
+		{
+			//Let scaryguy run
+			//scaryGuy.LERPDIEDERP
+
+			isCompleted = true;
+		}
+	}
+}
