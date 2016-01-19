53efcebc5d842bd5dc710dd23f5647148b8bb249
Choraimy <choraimy@gmail.com>
2016-01-19 12:38:59 +0100

    Added a triggerbox to Nimbi's flame breath so it can be detected Implemented new models and animations for Elephant and Hamster onesies

--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -108,7 +108,7 @@ namespace Nimbi.Actors {
         }
 
         private void SetSkeleton(string name) {
-            currentSkeleton = skeletons.Single(s => s.name == name);
+            currentSkeleton = skeletons.Single(s => s.name == "Onesie" + name);
             currentSkeleton.GetComponent<Animator>().SetTrigger("To_" + name);
             currentSkeleton.Renderer.enabled = true;
             skeletons.Where(s => s != currentSkeleton).ToList().ForEach(s => s.Renderer.enabled = false);
