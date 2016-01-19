8942644ccbbd9e0e4e14b5cda9fb4e93b8a8af77
Tim Ververs <timververs@msn.com>
2016-01-19 13:18:46 +0100

    Mensen zeggen dat ik op Matthijs van Nieuwkerk lijk

--- Assets/Scripts/Interaction/Captain.cs
+++ Assets/Scripts/Interaction/Captain.cs
@@ -12,12 +12,11 @@ namespace Nimbi.Interaction
         public GameObject landlobberDialog;
         public GameObject finishNutDialog;
         public GameObject fixBoatDialog;
+		public Transform endMarker;
 
-        private Player player;
+		private Player player;
         private GameObject nut;
-        private bool nutDelivered = false;
-        private Vector3 endMarker;
-        private float speed = 1f;
+        public bool nutDelivered = false;
         private float startTime;
         private float journeyLength;
         private bool firstInteraction = true;
@@ -25,22 +24,20 @@ namespace Nimbi.Interaction
         void Start()
         {
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-            endMarker = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z + 1f);
             nut = GameObject.Find("Nut");
-            journeyLength = Vector3.Distance(transform.position, endMarker);
         }
 
         void Update()
         {
             if (nutDelivered)
             {
-                float distCovered = (Time.time - startTime) * speed;
-                float fracJourney = distCovered / journeyLength;
-                transform.position = Vector3.MoveTowards(transform.position, endMarker, fracJourney);
+				GetComponent<Animator>().SetBool("walking", true);
+                transform.position = Vector3.MoveTowards(transform.position, endMarker.position, Time.deltaTime * 1.5f);
             }
-            if (transform.position == endMarker)
+            if (transform.position == endMarker.position)
             {
-                Destroy(this);
+				GetComponent<Animator>().SetBool("walking", false);
+				Destroy(this);
             }
         }
 
@@ -68,7 +65,6 @@ namespace Nimbi.Interaction
                 {
                     nutDelivered = true;
                     Destroy(nut);
-                    startTime = Time.time;
                     finishNutDialog.SetActive(true);
                     fixBoatDialog.SetActive(true);
 
