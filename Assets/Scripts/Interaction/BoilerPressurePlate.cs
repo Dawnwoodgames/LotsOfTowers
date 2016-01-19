787b64b7344b21cb04ecee5509d5addc39a7e2ae
JayDijkstra <jaydijkstra@gmail.com>
2016-01-19 12:42:38 +0100

    Added new model for boiler.. better then tim

---
+++ Assets/Scripts/Interaction/BoilerPressurePlate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors; 
+
+namespace Nimbi.Interaction
+{
+    public class BoilerPressurePlate : MonoBehaviour
+    {
+
+        private bool colliding;
+        private GameObject player;
+        // Use this for initialization
+
+        void Start()
+        {
+             
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(colliding == true)
+            {
+                print("Nimbi is standing on me");
+            }
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (colliding || collision.gameObject.tag != "Player") return;
+            colliding = true;
+        }
+    }
+}
+
