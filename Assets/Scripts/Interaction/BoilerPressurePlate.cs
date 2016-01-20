4f13fe42e9333eb7ba4c439bb2fb353a04b3d8e0
JayDijkstra <jaydijkstra@gmail.com>
2016-01-20 13:55:37 +0100

    Modified Firewood with Particles, Boiler Lid working with pressure plate.

--- Assets/Scripts/Interaction/BoilerPressurePlate.cs
+++ Assets/Scripts/Interaction/BoilerPressurePlate.cs
@@ -6,30 +6,73 @@ namespace Nimbi.Interaction
 {
     public class BoilerPressurePlate : MonoBehaviour
     {
+        public GameObject BoilerLid;
+        public int maxLidPosition = 50;
 
-        private bool colliding;
-        private GameObject player;
+        private bool inTrigger;
+        private Vector3 lidPosition; 
+        private Player player;
+        private bool lifting;
+        private Quaternion lidRotation;
+
+        private Vector3 rotateAngle;
+        
         // Use this for initialization
-
         void Start()
         {
-             
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            BoilerLid = GameObject.Find("Lid");
+
+            //Old Position values
+            lidRotation = transform.rotation;
+            
+            inTrigger = false;
+            lifting = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(colliding == true)
+            //If Nimbi is In his Elephant Onesie!
+            if (inTrigger == true && player.GetComponent<Player>().Onesie.isHeavy)
             {
-                print("Nimbi is standing on me");
+                lifting = true;
+
+                if (lifting)
+                {
+                    //Do Stuff
+                    Vector3 endPosition = new Vector3(-17, 0, 0);
+                    if (Vector3.Distance(BoilerLid.transform.eulerAngles, endPosition) > 0.01f)
+                    {
+                        if (BoilerLid.transform.eulerAngles.z < maxLidPosition)
+                        BoilerLid.transform.Rotate(-16, 0, 17);
+
+                    }
+
+                    else
+                    {
+                        BoilerLid.transform.Rotate(+17, 0, 0);
+                        print("Not Lifting Anymore");
+                    }
+
+                }
             }
         }
 
-        public void OnCollisionEnter(Collision collision)
+        public void OnTriggerStay(Collider col)
+            {
+                if (col.attachedRigidbody)
+                {
+                    inTrigger = true;
+                    lifting = false;
+            }
+            }
+        
+        public void OnTriggerExit()
         {
-            if (colliding || collision.gameObject.tag != "Player") return;
-            colliding = true;
+            inTrigger = false;
         }
-    }
+  
+     }
 }
 
---
+++ Assets/Scripts/Interaction/FireWood.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+using System.Linq;
+
+
+public class FireWood : MonoBehaviour {
+
+    public GameObject particle;
+
+    private Player player;
+    private bool hasFireContact;
+    private bool isTrigger;
+
+    
+
+// Use this for initialization
+    void Start () {
+    player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    hasFireContact = false;
+    isTrigger = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	 if(hasFireContact == true)
+        {
+            print("I am on fire Baby!");
+        
+        }
+	}
+
+    void Ignite()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Fire")
+        {
+            hasFireContact = true;
+            Ignite();
+        }
+    }
+
+
+
+
+
+}
