841ee717e5ca3f3656788cf5f94fa32ce51b70c0
Axel Schelfhout <axelschelfhout@gmail.com>
2016-01-22 09:20:10 +0100

    MOved all the fucking scripts you guys placed wrong

--- Assets/Scripts/Interaction/CypherPuzzleChecker.cs
+++ Assets/Scripts/Interaction/CypherPuzzleChecker.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace Nimbi
+namespace Nimbi.Interaction
 {
     public class CypherPuzzleChecker : MonoBehaviour
     {
---
+++ Assets/Scripts/Interaction/Triggers/FireLightTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class FireLightTrigger : MonoBehaviour
+    {
+        public Light dragonLight;
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Fire")
+            {
+                dragonLight.enabled = true;
+            }
+        }
+    }
+}
