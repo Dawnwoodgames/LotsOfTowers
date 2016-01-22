f23f5dab5bd10a0ac8bc0e5e865bb62f28e2b614
Lars <larsh1991@hotmail.com>
2016-01-22 13:56:10 +0100

    Marble puzzle completed

---
+++ Assets/Scripts/Interaction/Triggers/MarbleCompleteTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class MarbleCompleteTrigger : MonoBehaviour
+    {
+        private bool puzzleCompleted = false;
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.name == "Marble")
+                Complete();
+        }
+
+        private void Complete()
+        {
+            puzzleCompleted = true;
+        }
+
+        public bool GetPuzzleCompleted() { return puzzleCompleted; }
+    }
+}
