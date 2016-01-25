1beb1da67064750553a7c9a0602bdfad990a5c4b
Lars <larsh1991@hotmail.com>
2016-01-25 11:20:32 +0100

    Marble maze can not be wallhugged

---
+++ Assets/Scripts/Environment/MarbleTrackTransparancy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Environment
+{
+    public class MarbleTrackTransparancy : MonoBehaviour
+    {
+        private GameObject marbleTrack;
+        private Renderer rend;
+
+        void Start()
+        {
+            marbleTrack = transform.GetChild(0).gameObject;
+            rend = marbleTrack.GetComponent<Renderer>();
+            rend.material = rend.materials[0];
+        }
+
+        private void OnCollisionEnter(Collision coll)
+        {
+            if (coll.gameObject.tag == "Player")
+                ActivateSeethrough();
+        }
+
+        private void OnCollisionExit(Collision coll)
+        {
+            if (coll.gameObject.tag == "Player")
+                DeactivateSeethrough();
+        }
+
+        private void ActivateSeethrough()
+        {
+            rend.material = rend.materials[1];
+        }
+
+        private void DeactivateSeethrough()
+        {
+            rend.material = rend.materials[0];
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/CurrentLowersTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class CurrentLowersTrigger : MonoBehaviour
+    {
+        public Material lowersMat;
+
+        private Renderer rend;
+
+        void Start()
+        {
+            rend = GameObject.Find("WalkthroughFloor").GetComponent<Renderer>();
+        }
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+                ReplaceFloorWithImage();
+        }
+
+        private void ReplaceFloorWithImage()
+        {
+            rend.material = lowersMat;
+        }
+    }
+}
