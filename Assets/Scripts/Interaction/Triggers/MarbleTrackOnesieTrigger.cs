1cb9cb70d0637f884078942da0829c35a6395638
Lars <larsh1991@hotmail.com>
2016-01-25 15:18:52 +0100

    Disable onesie trigger added in Marble maze, Tower 5

---
+++ Assets/Scripts/Interaction/Triggers/MarbleTrackOnesieTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Nimbi.Actors;
+using System.Collections;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class MarbleTrackOnesieTrigger : MonoBehaviour
+    {
+
+        private Player player;
+
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        }
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+                player.PlayerCanSwitchOnesie = false;
+        }
+
+        private void OnTriggerExit(Collider coll)
+        {
+            if (coll.tag == "Player")
+                player.PlayerCanSwitchOnesie = true;
+        }
+    }
+}
