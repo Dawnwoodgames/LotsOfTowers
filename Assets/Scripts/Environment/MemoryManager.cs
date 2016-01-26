ff6d3de24858dee5b3cf543f581c1e1aa364d98c
Sephy <bob.vankamp@gmail.com>
2016-01-26 14:41:57 +0100

    Memory puzzle

---
+++ Assets/Scripts/Environment/MemoryManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Environment
+{
+    public class MemoryManager : MonoBehaviour
+    {
+        public MemoryStep[] steps;
+        public MemoryStairs[] stairs;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/MemoryStep.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Environment
+{
+    public class MemoryStep : MonoBehaviour
+    {
+        public MemoryManager manager;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
