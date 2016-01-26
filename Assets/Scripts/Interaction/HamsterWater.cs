a2bdafe28ef8d7c9aa8f0166a0ad48c33858ff4b
Sephy <bob.vankamp@gmail.com>
2016-01-26 13:13:07 +0100

    Lowered hamster water

--- Assets/Scripts/Interaction/HamsterWater.cs
+++ Assets/Scripts/Interaction/HamsterWater.cs
@@ -22,7 +22,7 @@ namespace Nimbi.Interaction
         {
             if (nearWater && Input.GetButton("Submit") && player.GetComponent<Player>().Onesie.isHeavy)
                 Spit();
-            gameObject.transform.localScale = Vector3.MoveTowards(gameObject.transform.localScale, new Vector3(gameObject.transform.localScale.x, spitcount * 0.05f, gameObject.transform.localScale.z), Time.deltaTime * 2);
+            gameObject.transform.localScale = Vector3.MoveTowards(gameObject.transform.localScale, new Vector3(gameObject.transform.localScale.x, spitcount * 0.03f, gameObject.transform.localScale.z), Time.deltaTime * 2);
             gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, new Vector3(gameObject.transform.localPosition.x, defaultPosition.y + spitcount * 0.1f, gameObject.transform.localPosition.z), Time.deltaTime * 2);
             if (spitcount == 0)
                 GetComponent<Renderer>().enabled = false;
