3fc91dde9d60b04249416c0561318530534d6696
Sephy <bob.vankamp@gmail.com>
2016-01-27 15:41:05 +0100

    Memory Handle trigger

---
+++ Assets/Scripts/Interaction/MemoryHandle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction
+{
+    public class MemoryHandle : MonoBehaviour
+    {
+
+        public GameObject rock;
+        private bool inTrigger;
+
+        void Update()
+        {
+            if(inTrigger && Input.GetButtonDown("Submit"))
+            {
+                if(rock!= null)
+                    Destroy(rock);
+            }
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+                inTrigger = true;
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.tag == "Player")
+                inTrigger = false;
+        }
+    }
+}
