403d4c39626a528a4eac3063f2a126222ae2ae90
Choraimy <choraimy@gmail.com>
2016-01-27 17:57:42 +0100

    Fixed zoom not working on controller

--- Assets/Scripts/CameraControl/CameraFollowScript.cs
+++ Assets/Scripts/CameraControl/CameraFollowScript.cs
@@ -17,9 +17,18 @@ namespace Nimbi.CameraControl
 
 		void Update()
 		{
+            // Mousewheel zoom (should probably rename mouse wheel axis to "Zoom" at some point)
 			if (Input.GetAxis("Mouse Wheel") != 0) {
                 cameraObject.transform.localPosition = new Vector3(0, 0, Mathf.Max(-15,
                     Mathf.Min(cameraObject.transform.localPosition.z + Input.GetAxis("Mouse Wheel"), -5)));
+            }
+
+            // Controller zoom
+            if (Input.GetAxis("Zoom") > 0.05f || Input.GetAxis("Zoom") < -0.05f) {
+                bool zoomIn = Input.GetAxis("Zoom") > 0;
+
+                cameraObject.transform.localPosition = new Vector3(0, 0, Mathf.Max(-15,
+                    Mathf.Min(cameraObject.transform.localPosition.z + (zoomIn ? 1 : -1), -5)));
             }
 
             transform.position = new Vector3(focus.transform.position.x, focus.transform.position.y + 0.5f, focus.transform.position.z);
