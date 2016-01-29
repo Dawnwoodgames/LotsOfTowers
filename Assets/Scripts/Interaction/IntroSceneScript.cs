9b80b9058eca51ed09256ee0e1f56e407ddbc634
Axel Schelfhout <axelschelfhout@gmail.com>
2016-01-29 10:30:35 +0100

    Johan. Learn2parse

--- Assets/Scripts/Interaction/IntroSceneScript.cs
+++ Assets/Scripts/Interaction/IntroSceneScript.cs
@@ -3,45 +3,34 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Nimbi.Interaction{ 
-public class IntroSceneScript : MonoBehaviour {
+namespace Nimbi.Interaction
+{ 
+    public class IntroSceneScript : MonoBehaviour
+    {
 
+	    public GameObject IntroText;
+	    public int IntroState = 0;
+	    public Sprite[] introSprites;
 
-	public GameObject IntroText;
-	public int IntroState = 0;
-	public Sprite[] introSprites;
-
-
-	void Awake(){
-
-	}
-
-	void Start () {
-			
-	}
-
-	void FixedUpdate () {
-
-	
-		switch (IntroState) {
-			case 8:
-                GameManager.Instance.LoadLevel(2);
-                break;
-            default:
-			    IntroText.GetComponent<Text>().text = ("intro_"+(IntroState+1)).Localize();
-			    GameObject.Find("ImageIntroScreen").GetComponent<Image>().sprite = introSprites[IntroState];
-			    break;
+	    void FixedUpdate () {
+            
+		    switch (IntroState) {
+			    case 8:
+                    GameManager.Instance.LoadLevel(2);
+                    break;
+                default:
+			        IntroText.GetComponent<Text>().text = ("intro_"+(IntroState+1)).Localize();
+			        GameObject.Find("ImageIntroScreen").GetComponent<Image>().sprite = introSprites[IntroState];
+			        break;
 		    
-		}
+		    }
 
-			if (Input.GetButtonDown ("Submit")) 
-			{
-				if (IntroState <8) {
-					IntroState++;
-				}
-
-			}
-
-	}
-}
+		    if (Input.GetButtonDown ("Submit")) 
+		    {
+		        if (IntroState <8) {
+			        IntroState++;
+		        }
+		    }
+	    }
+    }
 }
