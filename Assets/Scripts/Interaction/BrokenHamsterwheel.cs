10687a54dafaefcd9f1287f0b00db1ee9b82e30e
Lars <larsh1991@hotmail.com>
2016-02-10 14:50:23 +0100

    Tower 4 - Hamster wheel breaks consistent

--- Assets/Scripts/Interaction/BrokenHamsterwheel.cs
+++ Assets/Scripts/Interaction/BrokenHamsterwheel.cs
@@ -24,49 +24,50 @@ namespace Nimbi.Interaction
 
 		void FixedUpdate()
 		{
-			if (rotateTrigger.GetPlayerRunning() && !broken)
+            if (rotateTrigger.GetPlayerRunning() && !broken)
 			{
 				damage += 1 * Time.deltaTime;
 				if (damage > (maxDamage / 4))
 				{
-					transform.Rotate(new Vector3(0, 0, .05f));
+					transform.Rotate(new Vector3(0, 0, .15f));
 					if (damage > (maxDamage / 2))
 					{
-						transform.Rotate(new Vector3(0, 0, .15f));
+						transform.Rotate(new Vector3(0, 0, .25f));
 						if (damage > maxDamage)
 						{
-							waterHole.waterRising = false;
-							transform.Rotate(new Vector3(0, 0, .3f));
+							transform.Rotate(new Vector3(0, 0, .4f));
 						}
 					}
 				}
 			}
 			else if (!broken && damage > maxDamage)
 			{
-				if (player.Onesie.isHeavy && inTrigger)
+				if (player.Onesie.isHeavy)
 				{
 					broken = true;
 				}
 			}
 			else if (broken)
 			{
-				player.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * 20f, ForceMode.Impulse);
+                gameObject.AddComponent<Rigidbody>();
+                gameObject.GetComponent<Rigidbody>().mass = 0;
+                gameObject.GetComponent<Rigidbody>().drag = 0;
+                gameObject.GetComponent<Rigidbody>().useGravity = true;
+                gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+                gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
 
-				gameObject.AddComponent<Rigidbody>();
-				gameObject.GetComponent<Rigidbody>().useGravity = true;
-				gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-				gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                player.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-				gameObject.GetComponent<MeshCollider>().convex = true;
+                gameObject.GetComponent<MeshCollider>().convex = true;
 
-				Destroy(rotateTrigger.gameObject);
-				Destroy(transform.parent.GetChild(1).gameObject);
+                Destroy(rotateTrigger.gameObject);
+                Destroy(transform.parent.GetChild(1).gameObject);
                 Destroy(bumpWall);
-				Destroy(this);
-			}
+                Destroy(this);
+            }
 		}
 
-		private void OnTriggerStay(Collider coll)
+        private void OnTriggerStay(Collider coll)
 		{
 			if (coll.tag == "Player")
 			{
