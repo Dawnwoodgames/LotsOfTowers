bb7a13ec16da1c0cd8d4a371e85d67774b298738
Lars <larsh1991@hotmail.com>
2016-02-10 11:01:34 +0100

    Door at mirror puzzle at Tower 2 opens fluently

--- Assets/Scripts/Interaction/Door.cs
+++ Assets/Scripts/Interaction/Door.cs
@@ -4,35 +4,53 @@ using Nimbi.Audio;
 
 namespace Nimbi.Interaction
 {
-	public class Door : MonoBehaviour
-	{
-		public GameObject player;
-		public Vector3 unlockedPosition;
-		public GameObject key;
-		private bool inTrigger = false;
-		public GameObject mirrorDoor;
+    public class Door : MonoBehaviour
+    {
+        public GameObject originDoor;
 
-		void Update()
-		{
-			if (Input.GetButtonDown("Submit") && inTrigger)
-				if (player.GetComponentInChildren<MirrorKey>() != null)
+        private GameObject key;
+        private bool openingDoor = false;
+        private Quaternion originMarker, dupMarker;
+        private float speed = 1f;
+
+        public void Start()
+        {
+            key = GameObject.Find("Key").gameObject;
+
+            dupMarker = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 60, 0);
+            originMarker = Quaternion.Euler(0, originDoor.transform.rotation.eulerAngles.y - 60, 0);
+        }
+
+        private void Update()
+        {
+            if (openingDoor)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, dupMarker, Time.deltaTime * speed);
+                originDoor.transform.rotation = Quaternion.Lerp(originDoor.transform.rotation, originMarker, Time.deltaTime * speed);
+            }
+        }
+
+        private void OnTriggerStay(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                if (Input.GetButtonDown("Submit"))
                 {
-                    OpenDoor(key);
-                    GameObject.Find("PressurePlate").SetActive(false);
+                    if (key != null)
+                    {
+                        OpenDoor();
+                        GameObject.Find("PressurePlate").SetActive(false);
+                    }
                 }
-					
-                
-		}
+            }
+        }
 
-		private void OnTriggerStay(Collider coll) { inTrigger = true; }
-		private void OnTriggerExit() { inTrigger = false; }
+        private void OpenDoor()
+        {
+            AudioManager.Instance.PlaySoundeffect(AudioManager.Instance.doorOpenSoundFile);
+            Destroy(key);
 
-		private void OpenDoor(GameObject key)
-		{
-			AudioManager.Instance.PlaySoundeffect(AudioManager.Instance.doorOpenSoundFile);
-			Destroy(mirrorDoor);
-			Destroy(key);
-			Destroy(gameObject);
-		}
-	}
+            openingDoor = true;
+        }
+    }
 }
