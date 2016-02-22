96846ecaa062a31563a41f2c8a82e94d22810df9
Jay Dijkstra <jaydijkstra@gmail.com>
2016-02-22 17:08:24 +0100

    New Stuff for Tower 5 (New Door with Wheel) / Rock Explosion etc. (not spinning atm)

---
+++ Assets/Scripts/Interaction/Triggers/MaskRotationTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class MaskRotationTrigger : MonoBehaviour
+    {
+        float targetRotation = 360f; //Rotation to stop at
+        float distanceToSlow = 50f; //How far from target to start slowing down
+        float minDistance = 1f; //How far from target to stop completely
+
+        private float curAngle = 0f;
+        private float startAngle = 0f;
+        private float startTime = 0f;
+
+        public void Update()
+        {
+            float t = (Time.time - startTime);
+            curAngle = Mathf.Lerp(startAngle, targetRotation, t);
+        }
+
+
+
+    }
+
+  }
+
