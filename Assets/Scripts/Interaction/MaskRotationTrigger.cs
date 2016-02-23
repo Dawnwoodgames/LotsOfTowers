d2c402494557695769ec0ad086cb8d8398327bb7
JayDijkstra <jaydijkstra@gmail.com>
2016-02-23 14:38:59 +0100

    Changed Trigger for Mask Rotator

---
+++ Assets/Scripts/Interaction/MaskRotationTrigger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+
+namespace Nimbi.Interaction.Triggers
+{
+    public class MaskRotationTrigger : MonoBehaviour
+    {
+
+        public float rotationSpeed = 10f;
+        public float rotateBackSpeed = 10f;
+        public float pushBackRate = 30;
+
+        private GameObject player;
+        public bool isCheating;
+        private Quaternion startRotation;
+        private int rotationCount;
+
+        public bool isSpinning;
+        public bool isScary;
+        private bool inTrigger;
+
+
+        void Start()
+        {
+            GameObject.FindGameObjectWithTag("Player");
+            startRotation = transform.localRotation;
+        }
+
+        public void Update()
+        {
+
+            if (rotationSpeed > 0)
+            {
+                isSpinning = true;
+                transform.Rotate(rotationSpeed, 0, 0);
+            }
+
+            rotationSpeed -= Time.deltaTime / 1f;
+
+
+            if (rotationSpeed <= 0)
+            {
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, startRotation, 0.1f);
+                if (Mathf.Abs(transform.localRotation.eulerAngles.x - startRotation.eulerAngles.x) < 0.1f)
+                    rotationSpeed = 10;
+            }
+
+        }
+
+        public void PushNimbiAway()
+        {
+            player.GetComponent<Rigidbody>().AddForce(Vector3.right * pushBackRate, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Triggers/CheatingMaskTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+using Nimbi.Interaction.Triggers;
+
+namespace Nimbi.Actors.Triggers
+{
+    public class CheatingMaskTrigger : MonoBehaviour
+    {
+
+        private bool inTrigger;
+        private GameObject player;
+        public MaskRotationTrigger cheatingMask;
+
+        // Use this for initialization
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(inTrigger && cheatingMask.isCheating)
+            {
+                cheatingMask.PushNimbiAway();
+            }
+        }
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                Debug.Log("Whatttttt, player is hitting meeeeeeee?");
+                inTrigger = true;
+            }
+        }
+
+    }
+
+
+
+
+}
