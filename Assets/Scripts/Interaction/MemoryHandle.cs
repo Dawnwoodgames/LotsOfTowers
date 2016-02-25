0d5683b8ce4b929a56434e44b2a0a2adc9176773
JayDijkstra <jaydijkstra@gmail.com>
2016-02-25 13:34:01 +0100

    Rock gets Destroyed with MemoryPuzzleHandle

--- Assets/Scripts/Interaction/MemoryHandle.cs
+++ Assets/Scripts/Interaction/MemoryHandle.cs
@@ -7,15 +7,30 @@ namespace Nimbi.Interaction
     {
 
         public GameObject rock;
+        public GameObject handle;
         private bool inTrigger;
+
+        private Vector3 endRotation;
+
+
+        private bool isActivated;
+
+        void Start()
+        {
+            endRotation = new Vector3(314, 0, 0);
+        }
 
         void Update()
         {
-            if(inTrigger && Input.GetButtonDown("Submit"))
+            if(inTrigger && Input.GetButtonDown("Submit") && !isActivated)
             {
-                if(rock!= null)
+                endRotation = endRotation + new Vector3(55, 0, 0);
+                isActivated = true;
+                if (rock!= null)
                     Destroy(rock);
             }
+
+            handle.transform.localRotation = Quaternion.Slerp(handle.transform.localRotation, Quaternion.Euler(endRotation), 0.2f);
         }
 
         void OnTriggerEnter(Collider coll)
