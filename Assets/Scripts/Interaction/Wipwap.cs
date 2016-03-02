736cf4e3c77a81fa849e72aee891f5fac79abd07
Sephy <bob.vankamp@gmail.com>
2016-03-02 12:27:50 +0100

    Nieuwe wipwap

---
+++ Assets/Scripts/Interaction/Wipwap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Interaction
+{
+    public class Wipwap : MonoBehaviour
+    {
+
+        private bool inTrigger;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+                inTrigger = true;
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.tag == "Player")
+                inTrigger = false;
+        }
+    }
+}
