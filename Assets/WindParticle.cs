c6f674be39747b85d86b90b75cdf1b68f86c2ba3
Sephy <bob.vankamp@gmail.com>
2016-03-04 11:14:15 +0100

    Mooi wind man

---
+++ Assets/WindParticle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindParticle : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 200f;
+    public bool rotating = false;
+    public Vector3 direction;
+
+    private float chance;
+    private float angle;
+
+    void Start()
+    {
+        chance = 0;
+    }
+
+    void Update()
+    {
+        if (rotating)
+        {
+            angle += Time.deltaTime * rotationSpeed;
+            if (angle > 360)
+            {
+                rotating = false;
+                angle = 0;
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+                transform.localRotation = Quaternion.Euler(angle*-direction.z, angle*direction.y, angle*direction.x);
+
+        }
+        transform.Translate(direction * Time.deltaTime * speed); // move forward
+
+        if (!rotating)
+        {
+            if (Random.Range(0, 10000) < chance)
+            {
+                rotating = true;
+                chance = 0;
+            }
+            else
+                chance += 0.3f;
+        }
+    }
+}
---
+++ Assets/WindSpawnwer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindSpawnwer : MonoBehaviour {
+
+    public GameObject WindParticle;
+    public Vector3 direction;
+    public float spawnRate; //Time between spawns
+
+    float nextSpawn;
+	// Use this for initialization
+	void Start () {
+        nextSpawn = Time.time + spawnRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            GameObject wind = Instantiate(WindParticle) as GameObject;
+            wind.GetComponent<WindParticle>().direction = direction;
+        }
+	}
+}
