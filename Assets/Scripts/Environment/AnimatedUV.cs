5e8dbbec0cc4af455e772648a0b2ad0dbd239fbe
Lars H <Lars H>
2016-03-09 13:38:59 +0100

    Tower 4 has cartoon water implemented

---
+++ Assets/Scripts/Environment/AnimatedUV.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Nimbi.Environment
+{
+    public class AnimatedUV : MonoBehaviour
+    {
+        Vector2 uvOffset = Vector2.zero;
+
+        float scale = 0.1f;
+        float speed = 1.0f;
+        float noiseStrength = 1f;
+        float noiseWalk = 1f;
+
+        private Vector3[] baseHeight;
+
+        void Update()
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+            if (baseHeight == null)
+                baseHeight = mesh.vertices;
+
+            Vector3[] vertices = new Vector3[baseHeight.Length];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Vector3 vertex = baseHeight[i];
+                vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale;
+                vertex.y += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+                vertices[i] = vertex;
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateNormals();
+        }
+    }
+}
