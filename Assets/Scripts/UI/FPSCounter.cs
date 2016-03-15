a6519397f25a1237917fdd8ab6c1ead9e92826a1
Sephy <bob.vankamp@gmail.com>
2016-03-15 10:56:11 +0100

    Removed fps counter from the hud

--- Assets/Scripts/UI/FPSCounter.cs
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -5,22 +5,17 @@ using UnityEngine.UI;
 
 namespace Nimbi.UI
 {
-    [RequireComponent(typeof (Text))]
     public class FPSCounter : MonoBehaviour
     {
         const float fpsMeasurePeriod = 1.0f;
         private int m_FpsAccumulator = 0;
         private float m_FpsNextPeriod = 0;
         private int m_CurrentFps;
-        const string display = "{0}";
-        private Text m_Text;
 
 
         private void Start()
         {
             m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
-            m_Text = GetComponent<Text>();
-            Debug.Log("Start FPS Measurement");
             UnityAnalytics.StartFPSMeasurement();
         }
 
@@ -34,10 +29,6 @@ namespace Nimbi.UI
                 m_CurrentFps = (int) (m_FpsAccumulator/fpsMeasurePeriod);
                 m_FpsAccumulator = 0;
                 m_FpsNextPeriod += fpsMeasurePeriod;
-                if (Debug.isDebugBuild)
-                    m_Text.text = string.Format(display, m_CurrentFps);
-                else
-                    m_Text.text = "";
                 UnityAnalytics.AddFPSMeasurement(m_CurrentFps);
             }
         }
