7c13b400bce11c01bbb4d2cd6043e8dbc4e0c569
Sephy <bob.vankamp@gmail.com>
2016-03-16 12:07:34 +0100

    Vliegende vogels

---
+++ Assets/Scripts/Environment/Bird.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    public float speed = 5f;
+    public float rotationSpeed = 200f;
+    public Vector3 direction;
+    public Vector3 rotation;
+
+    private float angle;
+
+    void Update()
+    {
+            angle += Time.deltaTime * rotationSpeed;
+
+            transform.localRotation = Quaternion.Euler(rotation*angle);
+
+        transform.Translate(direction * Time.deltaTime * speed); // move forward
+    }
+}
