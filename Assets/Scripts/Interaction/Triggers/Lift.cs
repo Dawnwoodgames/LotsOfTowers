3a4b4d481537387053f64067440eef86209450ea
Sephy <bob.vankamp@gmail.com>
2016-03-30 12:51:18 +0200

    Liftscript gemaakt

--- Assets/Scripts/Interaction/Triggers/Lift.cs
+++ Assets/Scripts/Interaction/Triggers/Lift.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lift : MonoBehaviour {
+public class Lift : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    bool inTrigger;
+    public float speed = 1;
+    public Transform start;
+    public Transform end;
+    private bool goingup = true;
+    private bool goingdown;
+    private bool moving;
+
+    void Update()
+    {
+        if (moving)
+        {
+            if (transform.position.y < end.position.y && goingup)
+                transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            else if (transform.position.y > start.position.y && goingdown)
+                transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+
+            if (goingup && transform.position.y >= end.position.y)
+            {
+                moving = false;
+                goingdown = true;
+                goingup = false;
+            }
+            else if (goingdown && transform.position.y <= start.position.y)
+            {
+                moving = false;
+                goingup = true;
+                goingdown = false;
+            }
+
+            Debug.Log(goingdown);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        inTrigger = true;
+        moving = true;
+
+    }
+
+    void OnTriggerExit()
+    {
+        inTrigger = false;
+        if (moving)
+        {
+            goingdown = true;
+            goingup = false;
+        }
+    }
 }
