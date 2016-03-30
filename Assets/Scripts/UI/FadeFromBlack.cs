b1c0a9e5e1940d94edb8dd62909498142074190f
Sephy <bob.vankamp@gmail.com>
2016-03-30 10:44:27 +0200

    Script toegevoegd voor de fade vanuit zwart

---
+++ Assets/Scripts/UI/FadeFromBlack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FadeFromBlack : MonoBehaviour {
+
+    Image img;
+    float levelstart;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        img.color = Color.black;
+        levelstart = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Color c = img.color;
+        c.a -= ((Time.time-levelstart)*0.005f);
+        img.color = c;
+        Debug.Log(c.a);
+	}
+}
