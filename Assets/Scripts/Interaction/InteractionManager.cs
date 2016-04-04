d4938704b3a40faf8968883b3352b611d3ce7e50
Sephy <bob.vankamp@gmail.com>
2016-04-04 10:28:11 +0200

    Brievenbus kan geopend worden

---
+++ Assets/Scripts/Interaction/InteractionManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionManager : MonoBehaviour
+{
+
+    public Animator interactableObject;
+    bool inTrigger;
+
+    void Update()
+    {
+        if (inTrigger && Input.GetButtonDown("Submit"))
+            interactableObject.SetTrigger("Interact");
+    }
+
+    void OnTriggerEnter()
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit()
+    {
+        inTrigger = false;
+    }
+}
