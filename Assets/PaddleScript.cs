a6011643a2916a4b676c7ff731a9ab9b5c59f54f
Jay Dijkstra <jaydijkstra@gmail.com>
2016-04-04 16:51:11 +0200

    g g g g g gerrrriiiittttt

---
+++ Assets/PaddleScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nimbi.Actors;
+
+namespace Nimbi.Interaction {
+
+public class PaddleScript : MonoBehaviour {
+
+
+private bool inTrigger;
+private Collider paddleCol;
+
+public void Start()
+{
+    
+}
+
+public void Update(){
+if(inTrigger && Input.GetButtonDown("Submit"))
+{
+            this.transform.parent = paddleCol.transform;
+            this.transform.localPosition = new Vector3(0, 2.0f);
+  }
+}
+
+public void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            paddleCol = coll;
+            inTrigger = true;
+        }
+    }
+}
