10e8d8c56de17646d82bbce27ddd1228f85c51ee
Sephy <bob.vankamp@gmail.com>
2016-04-05 14:05:50 +0200

    Levelsliders toegevoegd

--- Assets/Scripts/Framework/LevelSlider.cs
+++ Assets/Scripts/Framework/LevelSlider.cs
@@ -15,32 +15,33 @@ namespace Nimbi.Framework
         [HideInInspector]
         public bool InTrigger = false;
         private bool[] isActive;
-        
-        void Start()
-		{
-			oldPosition = new Vector3[toMove.Length];
-            isActive = new bool[toMove.Length];
-			for (int i = 0; i < toMove.Length; i++)
-			    oldPosition[i] = toMove[i].transform.position;
-            
-		}
 
-		void Update()
-		{
-			if (InTrigger) { 
-				for (int obj = 0; obj < toMove.Length; obj++)
-				{
+        void Start()
+        {
+            oldPosition = new Vector3[toMove.Length];
+            isActive = new bool[toMove.Length];
+            for (int i = 0; i < toMove.Length; i++)
+                oldPosition[i] = toMove[i].transform.position;
+
+        }
+
+        void Update()
+        {
+            if (InTrigger)
+            {
+                for (int obj = 0; obj < toMove.Length; obj++)
+                {
                     if (!isActive[obj])
                         continue;
-                    toMove[obj].transform.position = Vector3.MoveTowards(toMove[obj].transform.position, new Vector3(oldPosition[obj].x, oldPosition[obj].y+increaseBy, oldPosition[obj].z), Time.deltaTime * speed);
-                    if (toMove[obj].transform.position == new Vector3(oldPosition[obj].x, oldPosition[obj].y+increaseBy, oldPosition[obj].z))
+                    toMove[obj].transform.position = Vector3.MoveTowards(toMove[obj].transform.position, new Vector3(oldPosition[obj].x, oldPosition[obj].y + increaseBy, oldPosition[obj].z), Time.deltaTime * speed);
+                    if (toMove[obj].transform.position == new Vector3(oldPosition[obj].x, oldPosition[obj].y + increaseBy, oldPosition[obj].z))
                         isActive[obj] = false;
                 }
             }
             else
             {
-				for (int obj = 0; obj < toMove.Length; obj++)
-				{
+                for (int obj = 0; obj < toMove.Length; obj++)
+                {
                     if (!isActive[obj])
                         continue;
                     toMove[obj].transform.position = Vector3.MoveTowards(toMove[obj].transform.position, new Vector3(oldPosition[obj].x, oldPosition[obj].y, oldPosition[obj].z), Time.deltaTime * speed);
@@ -50,24 +51,29 @@ namespace Nimbi.Framework
             }
         }
 
-		void OnTriggerEnter(Collider col)
-		{
-			if (col.tag == "Player" || col.tag == "HamsterBall")
-			{
-				InTrigger = true;
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.tag == "Player" || col.tag == "HamsterBall")
+            {
+                InTrigger = true;
                 for (int i = 0; i < toMove.Length; i++)
                     isActive[i] = true;
             }
-		}
+        }
 
-		void OnTriggerExit(Collider col)
-		{
-			if (col.tag == "Player" || col.tag == "HamsterBall")
-			{
-				InTrigger = false;
+        void OnTriggerExit(Collider col)
+        {
+            if (col.tag == "Player" || col.tag == "HamsterBall")
+            {
+                InTrigger = false;
                 for (int i = 0; i < toMove.Length; i++)
                     isActive[i] = true;
             }
-		}
-	}
+        }
+
+        void Reset()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+    }
 }
